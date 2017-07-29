15e70678ef11d90e2c574fb3fd01158908b2687f
Liz Parody <lizparody@gmailo.com>
2017-07-29 16:32:35 +0000

    added the first assignment

--- football_dictionaries/assignment_1.py
+++ football_dictionaries/assignment_1.py
@@ -1,2 +1,6 @@
 def players_as_dictionaries(squads_list):
-    pass
+  my_list = []
+  for value in squads_list:
+    my_dict = {'number': value[0], 'position': value[1], 'name': value[2], 'date_of_birth': value[3], 'caps': value[4], 'club': value[5], 'country': value[6], 'club_country': value[7],'year': value[8]}
+    my_list.append(my_dict)
+  return my_list
