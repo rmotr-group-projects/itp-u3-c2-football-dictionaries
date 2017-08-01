dd70816052691038743ed4613f52852d791a5fe0
Brett Wildblood <tensai2bai@gmail.com>
2017-08-01 00:52:20 +0000

    completed project and fixed test

--- football_dictionaries/assignment_2.py
+++ football_dictionaries/assignment_2.py
@@ -1,2 +1,23 @@
 def players_by_position(squads_list):
-    pass
+    result = {}
+    for player in squads_list:
+        player_dict = {
+        'number': player[0],
+        'position': player[1],
+        'name': player[2],
+        'date_of_birth': player[3],
+        'caps': player[4],
+        'club': player[5],
+        'country': player[6],
+        'club_country': player[7],
+        'year': player[8]
+        }
+        if player[1] in result :
+            result[player[1]].append(player_dict)
+        else:
+            result[player[1]] = [player_dict]
+    
+    return result
+    
+    
+    
