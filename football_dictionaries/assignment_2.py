ebfcab2bc2de3fe243bcb2f8829069f30ccaa213
Devan Govender <devakumarangovender@gmail.com>
2017-08-02 05:03:01 +0000

    Assignments 1 & 2

--- football_dictionaries/assignment_2.py
+++ football_dictionaries/assignment_2.py
@@ -1,2 +1,26 @@
 def players_by_position(squads_list):
-    pass
+    
+    field_list = ['number', 
+                  'position',
+                  'name',
+                  'date_of_birth',
+                  'caps',
+                  'club', 
+                  'country', 
+                  'club_country',
+                  'year'
+                 ]
+    
+    output_list = []
+    for player in squads_list:
+        output_list.append(dict(zip(field_list, list(player[:]))))
+    
+    output_dict = dict()
+    
+    for player in output_list:
+        if player["position"] not in output_dict:
+            output_dict[player["position"]] = []
+        output_dict[player["position"]].append(player)
+    
+    return output_dict
+    
