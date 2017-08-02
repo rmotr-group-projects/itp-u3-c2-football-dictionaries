bf58dcc4d8d0482e409e97a7156a1322380df866
Liz Parody <lizparody@gmail.com>
2017-08-02 17:56:58 -0400

    change the style the assignment 3

--- football_dictionaries/assignment_3.py
+++ football_dictionaries/assignment_3.py
@@ -6,7 +6,7 @@ def players_by_country_and_position(squads_list):
         "MF" : [],
         "FW" : []
         }
-  
+
   for squads in squads_list:
     my_dict = {'number': squads[0],
     'position': squads[1],
@@ -18,11 +18,11 @@ def players_by_country_and_position(squads_list):
     'club_country': squads[7],
     'year': squads[8]
     }
-      
+
     player_country = my_dict["country"]
     player_position = my_dict["position"]
     if player_country not in players_by_country:
         players_by_country[player_country] = players_by_position
     players_by_country[player_country][player_position].append(my_dict)
-        
+
   return players_by_country
