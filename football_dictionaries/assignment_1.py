65f00f830aa1e6eacf81622bc82b0a120ce627c9
Eric Richardson <erichardson919@gmail.com>
2017-09-08 13:08:06 +0000

    changed list to tuple and added comments

--- football_dictionaries/assignment_1.py
+++ football_dictionaries/assignment_1.py
@@ -1,7 +1,11 @@
 def players_as_dictionaries(squads_list):
     list_of_players = []
-    key_tuple = ['number', 'position', 'name', 'date_of_birth', 'caps', 'club', 'country', 'club_country', 'year']
+    
+    # Created a tuple of keys because these will not change and for use with the
+    # zip() function
+    key_tuple = ('number', 'position', 'name', 'date_of_birth', 'caps', 'club', 'country', 'club_country', 'year')
     for players in squads_list:
+        
         # Uses the zip on the key_list above and players
         dict_of_players_stats = dict(zip(key_tuple,players))
         #dict_of_players_stats['number'] = players[0]
@@ -13,6 +17,7 @@ def players_as_dictionaries(squads_list):
         #dict_of_players_stats['country'] = players[6]
         #dict_of_players_stats['club_country'] = players[7]
         #dict_of_players_stats['year'] = players[8]
+        
         # Add player stat into the list of players
         list_of_players.append(dict_of_players_stats)
     
