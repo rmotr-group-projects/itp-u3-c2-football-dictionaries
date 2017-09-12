84faf3d647df6ff9627b7d76c985c423e4db7c61
Eric Richardson <erichardson919@gmail.com>
2017-09-12 12:15:27 +0000

    reordered where functions were placed for readability

--- football_dictionaries/assignment_2.py
+++ football_dictionaries/assignment_2.py
@@ -1,3 +1,4 @@
+# Import assignment 1 to do assignment 2 
 from assignment_1 import players_as_dictionaries
 
 def players_by_position(squads_list):
@@ -5,20 +6,16 @@ def players_by_position(squads_list):
     # Unique names of all positions in list
     position_set = set()
     
-    # This will contain the players organized by position
-    
-    position_dictionary = {}
-    
-    key_tuple = ('number', 'position', 'name', 'date_of_birth', 'caps', 'club', 'country', 'club_country', 'year')
-    
     # Find an add all unique positions to position_set 
     for players in squads_list:
         position_set.add(players[1])
     
+    # This will contain the players organized by position
+    position_dictionary = {}
+    
     # Go through positions that are in the list 
     for position in position_set:
-        # Create a list that will be the value per position key
-        ##position_dictionary[position] =[]
+        # Create a list of players for a certain position
         position_list = []
         print(type(position_dictionary))
         # Go through players in the squads_list 
