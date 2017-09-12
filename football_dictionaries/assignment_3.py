d2d7143ff67881215d71da937e0cc40b9a91b5bf
Eric Richardson <erichardson919@gmail.com>
2017-09-12 23:03:39 +0000

    fully working assignment_3

--- football_dictionaries/assignment_3.py
+++ football_dictionaries/assignment_3.py
@@ -5,17 +5,19 @@ def players_by_country_and_position(squads_list):
     
     # Creating a unique set of countries from the list    
     country_set=set()
+    
     for players in squads_list:
         country_set.add(players[6])
-        
+    country_list = list(country_set)
+    country_list = sorted(country_list)
+    print (country_list)
     country_roster_dictionary = {}
-
-    #for country in country_set:
-    #    country_player_list =[]
-    #    for player in squads_list:
-    #        if country in player:
-    #            country_player_list.append(player)
-    #    country_roster_dictionary[country] = players_by_position(country_player_list)
+    for country in country_list:
+        print(country)
+        country_player_list = []
+        for player in squads_list:
+            if country in player:
+                country_player_list.append(player)
+        country_roster_dictionary[country] = players_by_position(country_player_list)
     
-    pass    
     return country_roster_dictionary
