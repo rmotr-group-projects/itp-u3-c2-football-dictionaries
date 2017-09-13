b39efc66c28bc7a6f6601d1c00bcc7b2a69505ee
Phuc Tran <chubby018@gmail.com>
2017-09-13 00:47:24 +0000

    possible fix for 3.5/3.6

--- football_dictionaries/assignment_2.py
+++ football_dictionaries/assignment_2.py
@@ -1,4 +1,4 @@
-from assignment_1 import players_as_dictionaries
+from .assignment_1 import players_as_dictionaries
 
 def players_by_position(squads_list):
     squad_list = players_as_dictionaries(squads_list)
