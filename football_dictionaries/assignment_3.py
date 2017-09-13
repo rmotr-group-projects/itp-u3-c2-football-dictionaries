3b4eba88468f58704ec885e1526d18d3ce6f4afd
Phuc Tran <chubby018@gmail.com>
2017-09-13 00:33:35 +0000

    completed all assignments

--- football_dictionaries/assignment_3.py
+++ football_dictionaries/assignment_3.py
@@ -1,2 +1,15 @@
+from assignment_1 import players_as_dictionaries
+from collections import OrderedDict
+
 def players_by_country_and_position(squads_list):
-    pass
+    squad_list = players_as_dictionaries(squads_list)
+    squads_by_country = OrderedDict()                                      
+    
+    for player in squad_list:
+        player_country = player['country']
+        player_position = player['position']
+        squads_by_country.setdefault(player_country, {})
+        squads_by_country[player_country].setdefault(player_position, [])
+        squads_by_country[player_country][player_position].append(player)
+        
+    return squads_by_country
