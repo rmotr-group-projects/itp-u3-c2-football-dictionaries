01453d03c47eb9d103d04572d9ac59e5d04b1df8
Faith <fwallacephd@gmail.com>
2017-10-21 19:12:58 +0000

    completed 102117

--- football_dictionaries/assignment_1.py
+++ football_dictionaries/assignment_1.py
@@ -1,2 +1,16 @@
 def players_as_dictionaries(squads_list):
-    pass
+    squad_dictionary = []
+    for player in squads_list:
+        player_dictionary = {
+            'number': player[0],
+            'position' : player[1],
+            'name': player[2],
+            'date_of_birth': player[3],
+            'caps': player[4],
+            'club': player[5],
+            'country': player[6],
+            'club_country': player[7],
+            'year': player[8],
+        }
+        squad_dictionary.append(player_dictionary)
+    return squad_dictionary
--- football_dictionaries/assignment_3.py
+++ football_dictionaries/assignment_3.py
@@ -1,2 +1,31 @@
+def players_as_dictionaries(squads_list):
+    squad_dictionary = []
+    for player in squads_list:
+        player_dictionary = {
+            'number': player[0],
+            'position' : player[1],
+            'name': player[2],
+            'date_of_birth': player[3],
+            'caps': player[4],
+            'club': player[5],
+            'country': player[6],
+            'club_country': player[7],
+            'year': player[8],
+        }
+        squad_dictionary.append(player_dictionary)
+    return squad_dictionary
+
 def players_by_country_and_position(squads_list):
-    pass
+    player_country = {
+        
+    }
+    for player in players_as_dictionaries(squads_list):
+        country = player["country"]
+        position = player["position"]
+        if country not in player_country:
+            player_country[country] = {}
+        if position not in player_country[country]:
+            player_country[country][position] = []
+        player_country[country][position].append(player)
+    return player_country
+  
