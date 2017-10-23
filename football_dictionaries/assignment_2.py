867f8ee8bfe0a94e0151a26b31ecae05dcd24510
yeli-buonya <yeli.e.buonya@gmail.com>
2017-10-22 20:39:38 -0700

    update file

--- football_dictionaries/assignment_2.py
+++ football_dictionaries/assignment_2.py
@@ -1,22 +1,20 @@
-from assignment_1 import players_as_dictionaries
-from squads_data import SQUADS_DATA
-from pprint import pprint
-
-
 def players_by_position(squads_list):
     positions = []
     positions_list = {}
 
+    # gets positions from all the players
     for player in squads_list:
         player_position = player.get('position')
         positions.append(player_position)
     
+    # sets default positions for positions list
     for position in positions:
         positions_list.setdefault(position, [])
 
+    # gets each position from players
     for player in squads_list:
         player_position = player.get('position')
-
+        # if position matches, add the player to that group
         for position in positions_list:
             if player_position == position:
                 positions_list[player_position].append(player)
