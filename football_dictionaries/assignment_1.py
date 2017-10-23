32f1c1a4752e9f2e01c5bde2910ede45a399d43a
Adriana <aspatterson@gmail.com>
2017-10-23 19:52:33 +0000

    Football dictionary solutions

--- football_dictionaries/assignment_1.py
+++ football_dictionaries/assignment_1.py
@@ -1,2 +1,241 @@
-def players_as_dictionaries(squads_list):
-    pass
+import csv
+
+SQUADS_DATA = [
+  [
+    "1",
+    "GK",
+    "Juan Botasso",
+    "(1908-10-23)23 October 1908 (aged 21)",
+    "",
+    "Quilmes",
+    "Argentina",
+    "Argentina",
+    "1930"
+  ],
+  [
+    "9",
+    "FW",
+    "Roberto Cherro",
+    "(1907-02-23)23 February 1907 (aged 23)",
+    "",
+    "Boca Juniors",
+    "Argentina",
+    "Argentina",
+    "1930"
+  ],
+  [
+    "-",
+    "MF",
+    "Pierre Braine",
+    "(1900-10-26)26 October 1900 (aged 29)",
+    "42",
+    "Royal Beerschot AC",
+    "Belgium",
+    "Belgium",
+    "1930"
+  ],
+  [
+    "-",
+    "MF",
+    "Alexis Chantraine",
+    "(1901-03-16)16 March 1901 (aged 29)",
+    "0",
+    "Royal FC Liegeois",
+    "Belgium",
+    "Belgium",
+    "1930"
+  ],
+  [
+    "-",
+    "GK",
+    "Jean De Bie",
+    "(1892-05-09)9 May 1892 (aged 38)",
+    "37",
+    "Royal Racing Club de Bruxelles",
+    "Belgium",
+    "Belgium",
+    "1930"
+  ],
+  [
+    "-",
+    "MF",
+    "Oscar",
+    "(1991-09-09)9 September 1991 (aged 22)",
+    "29",
+    "Chelsea",
+    "Brazil",
+    "England",
+    "2010"
+  ],
+  [
+    "-",
+    "MF",
+    "Paulinho",
+    "(1988-07-25)25 July 1988 (aged 25)",
+    "25",
+    "Tottenham Hotspur",
+    "Brazil",
+    "England",
+    "2010"
+  ],
+  [
+    "-",
+    "MF",
+    "Hernanes",
+    "(1985-05-29)29 May 1985 (aged 29)",
+    "23",
+    "Internazionale",
+    "Brazil",
+    "Italy",
+    "2014"
+  ],
+  [
+    "-",
+    "MF",
+    "Luiz Gustavo",
+    "(1987-07-23)23 July 1987 (aged 26)",
+    "17",
+    "VfL Wolfsburg",
+    "Brazil",
+    "Germany",
+    "2014"
+  ],
+  [
+    "-",
+    "MF",
+    "Fernandinho",
+    "(1985-05-04)4 May 1985 (aged 29)",
+    "6",
+    "Manchester City",
+    "Brazil",
+    "England",
+    "2014"
+  ],
+  [
+    "-",
+    "MF",
+    "Willian",
+    "(1988-08-09)9 August 1988 (aged 25)",
+    "5",
+    "Chelsea",
+    "Brazil",
+    "England",
+    "2014"
+  ],
+  [
+    "-",
+    "FW",
+    "Lee Keun-Ho",
+    "(1985-04-11)11 April 1985 (aged 29)",
+    "62",
+    "Sangju Sangmu",
+    "South Korea",
+    "South Korea",
+    "2014"
+  ],
+  [
+    "-",
+    "FW",
+    "Koo Ja-Cheol",
+    "(1989-02-27)27 February 1989 (aged 25)",
+    "35",
+    "Mainz 05",
+    "South Korea",
+    "Germany",
+    "2014"
+  ],
+  [
+    "-",
+    "FW",
+    "Kim Shin-Wook",
+    "(1988-04-14)14 April 1988 (aged 26)",
+    "26",
+    "Ulsan Hyundai",
+    "South Korea",
+    "South Korea",
+    "2014"
+  ]
+]
+
+squad = []
+# Thought we had to import a CSV, so code will work with both imported csv file and "SQUAD_DATA" listed above
+def read_squad_file(file_name='squads.csv'):
+
+    with open(file_name) as fp:
+        reader = csv.reader(fp)
+        for line in reader:
+            squad.append(line)
+    return squad
+
+
+def players_as_dictionaries(sq1):
+#    read_squad_file()
+    keys = [
+    'number',
+    'position',
+    'name',
+    'date_of_birth',
+    'caps',
+    'club',
+    'country',
+    'club_country',
+    'year',
+]
+    player_list = [ ]
+
+    for i in range(len(sq1)):
+        result = zip(keys,sq1[i])
+        player_dict = { }
+        for i in range(len(result)):
+            player_dict[result[i][0]] = result[i][1] 
+        player_list.append(player_dict)
+    return player_list
+
+
+
+
+
+print(players_as_dictionaries(SQUADS_DATA))
+
+
+#TESTS
+def test_assignment_1():
+    result = players_as_dictionaries(SQUADS_DATA)
+    assert len(result) == 14
+
+    assert result[0] == {
+        'caps': '',
+        'club': 'Quilmes',
+        'club_country': 'Argentina',
+        'country': 'Argentina',
+        'date_of_birth': '(1908-10-23)23 October 1908 (aged 21)',
+        'name': 'Juan Botasso',
+        'number': '1',
+        'position': 'GK',
+        'year': '1930'
+    }
+
+    assert result[1] == {
+        'caps': '',
+        'club': 'Boca Juniors',
+        'club_country': 'Argentina',
+        'country': 'Argentina',
+        'date_of_birth': '(1907-02-23)23 February 1907 (aged 23)',
+        'name': 'Roberto Cherro',
+        'number': '9',
+        'position': 'FW',
+        'year': '1930'
+    }
+
+    assert result[-1] == {
+        'caps': '26',
+        'club': 'Ulsan Hyundai',
+        'club_country': 'South Korea',
+        'country': 'South Korea',
+        'date_of_birth': '(1988-04-14)14 April 1988 (aged 26)',
+        'name': 'Kim Shin-Wook',
+        'number': '-',
+        'position': 'FW',
+        'year': '2014'
+    }
+    
--- football_dictionaries/assignment_3.py
+++ football_dictionaries/assignment_3.py
@@ -1,2 +1,186 @@
-def players_by_country_and_position(squads_list):
-    pass
+import pprint
+from squads_data import SQUADS_DATA  
+from assignment_1 import players_as_dictionaries
+
+
+
+def players_by_country_and_position(arg1):
+    arg2 = players_as_dictionaries(arg1)
+    
+    Argentina = []
+    Belgium = []
+    Brazil = []
+    SKorea = []
+    
+    for i in range(len(arg2)):
+        country = arg2[i].get('country')
+        
+        if country == 'Argentina':
+            Argentina.append(arg2[i])
+            a_goalkeepers = []
+            a_midfielders = []
+            a_forwards = []
+            for i in range(len(Argentina)):
+               player_pos_a = Argentina[i].get('position')
+               if player_pos_a == 'GK':
+                   a_goalkeepers.append(Argentina[i])
+               elif player_pos_a == 'MF':
+                   a_midfielders.append(Argentina[i])
+               elif player_pos_a == 'FW':
+                   a_forwards.append(Argentina[i])
+              
+        elif country == 'Belgium':
+            Belgium.append(arg2[i])
+            be_goalkeepers = []
+            be_midfielders = []
+            be_forwards = []
+            for i in range(len(Belgium)):
+                player_pos_be = Belgium[i].get('position')
+                if player_pos_be == 'GK':
+                    be_goalkeepers.append(Belgium[i])
+                elif player_pos_be == 'MF':
+                    be_midfielders.append(Belgium[i])
+                elif player_pos_be == 'FW':
+                    be_forwards.append(Belgium[i])           
+        
+        elif country == 'Brazil':
+            Brazil.append(arg2[i])
+            br_goalkeepers = []
+            br_midfielders = []
+            br_forwards = []
+            for i in range(len(Brazil)):
+                player_pos_br = Brazil[i].get('position')
+                if player_pos_br == 'GK':
+                    br_goalkeepers.append(Brazil[i])
+                elif player_pos_br == 'MF':
+                    br_midfielders.append(Brazil[i])
+                elif player_pos_br == 'FW':
+                    br_forwards.append(Brazil[i])
+        
+        elif country == 'South Korea':
+            SKorea.append(arg2[i])
+            sk_goalkeepers = []
+            sk_midfielders = []
+            sk_forwards = []
+            for i in range(len(SKorea)):
+                player_pos_sk = SKorea[i].get('position')
+                if player_pos_sk == 'GK':
+                    sk_goalkeepers.append(SKorea[i])
+                elif player_pos_sk == 'MF':
+                    sk_midfielders.append(SKorea[i])
+                elif player_pos_sk == 'FW':
+                    sk_forwards.append(SKorea[i])
+    
+    arg_dict = {}
+    arg_dict['GK'] = a_goalkeepers
+    if a_midfielders == []:
+        pass
+    else:
+        arg_dict['MF'] = a_midfielders
+    arg_dict['FW'] = a_forwards
+    
+    bra_dict = {}
+    if br_goalkeepers == []:
+        pass
+    else:
+        bra_dict['GK'] = br_goalkeepers
+    bra_dict['MF'] = br_midfielders
+    if br_forwards == []:
+        pass
+    else:
+        bra_dict['GK'] = br_forwards
+    
+    
+    bel_dict = {}
+    bel_dict['GK'] = be_goalkeepers
+    bel_dict['MF'] = be_midfielders
+    bel_dict['FW'] = be_forwards
+    
+    sko_dict = {}
+    sko_dict['GK'] = sk_goalkeepers
+    sko_dict['MF'] = sk_midfielders
+    sko_dict['FW'] = sk_forwards
+    
+    all_country_dict = {}
+    all_country_dict['Argentina'] = arg_dict
+    all_country_dict['Brazil'] = bra_dict
+    all_country_dict['Belgium'] = bel_dict
+    all_country_dict['South Korea'] = sko_dict
+    
+    return all_country_dict
+'''  
+        if country == 'Argentina':
+            Argentina.append(arg2[i])
+            a_goalkeepers = []
+            a_midfielders = []
+            a_forwards = []
+            for i in range(len(arg2)):
+                player_pos = arg2[i].get('position')
+                if player_pos == 'GK':
+                    a_goalkeepers.append(arg2[i])
+                elif player_pos == 'MF':
+                    a_midfielders.append(arg2[i])
+                elif player_pos == 'FW':
+                    a_forwards.append(arg2[i]) 
+
+
+
+    arg_dict = {}
+    arg_dict['GK'] = a_goalkeepers
+    arg_dict['MF'] = a_midfielders
+    arg_dict['FW'] = a_forwards
+    
+    
+    all_country_dict = {}
+    all_country_dict['Argentina'] = arg_dict
+#    all_country_dict['Belgium'] = bel_dict
+    
+    return all_country_dict
+'''   
+
+
+
+'''   
+        
+        elif country == 'Brazil':
+            Brazil.append(arg1[i])
+            br_goalkeepers = []
+            br_midfielders = []
+            br_forwards = []
+            for i in range(len(arg1)):
+                player_pos = arg1[i].get('position')
+                if player_pos == 'GK':
+                    br_goalkeepers.append(arg1[i])
+                elif player_pos == 'MF':
+                    br_midfielders.append(arg1[i])
+                elif player_pos == 'FW':
+                    br_forwards.append(arg1[i])
+        elif country == 'South Korea':
+            South_Korea.append(arg1[i])
+    '''
+#    arg_dict = {}
+#    arg_dict['GK'] = a_goalkeepers
+#    arg_dict['MF'] = a_midfielders
+#    arg_dict['FW'] = a_forwards
+   
+#    bel_dict = {}
+#    bel_dict['GK'] = be_goalkeepers
+#    bel_dict['MF'] = be_midfielders
+#    bel_dict['FW'] = be_forwards
+    
+#    bra_dict = {}
+#    bra_dict['GK'] = br_goalkeepers
+#    bra_dict['MF'] = br_midfielders
+#    bra_dict['FW'] = br_forwards 
+    
+#    all_country_dict = {}
+#    all_country_dict['Argentina'] = arg_dict
+#    all_country_dict['Belgium'] = bel_dict
+#    all_country_dict['Brazil'] = br_forwards
+#    all_country_dict['South_Korea'] = South_Korea
+    
+#    return arg_dict
+
+
+pp = pprint.PrettyPrinter(depth=6)
+pp.pprint(players_by_country_and_position(SQUADS_DATA))
