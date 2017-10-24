1b8a26acdd4bb7f24b862b00882225d512dd4278
yeli-buonya <yeli.e.buonya@gmail.com>
2017-10-23 19:03:47 -0700

    update files

--- football_dictionaries/assignment_2.py
+++ football_dictionaries/assignment_2.py
@@ -1,18 +1,22 @@
+from assignment_1 import players_as_dictionaries
+
+
 def players_by_position(squads_list):
     positions = []
     positions_list = {}
+    formatted_list = players_as_dictionaries(squads_list)
 
     # gets positions from all the players
-    for player in squads_list:
+    for player in formatted_list:
         player_position = player.get('position')
         positions.append(player_position)
-    
+
     # sets default positions for positions list
     for position in positions:
         positions_list.setdefault(position, [])
 
     # gets each position from players
-    for player in squads_list:
+    for player in formatted_list:
         player_position = player.get('position')
         # if position matches, add the player to that group
         for position in positions_list:
@@ -20,4 +24,3 @@ def players_by_position(squads_list):
                 positions_list[player_position].append(player)
 
     return positions_list
-
--- football_dictionaries/assignment_3.py
+++ football_dictionaries/assignment_3.py
@@ -1,2 +1,24 @@
+from assignment_1 import players_as_dictionaries
+from assignment_2 import players_by_position
+
+
 def players_by_country_and_position(squads_list):
-    pass
+    countries = {}
+    # get results from previous assignments
+    formatted_list = players_as_dictionaries(squads_list)
+    positions_list = players_by_position(squads_list)
+
+    # iterate through the formatted list of players
+    for player in formatted_list:
+        # add countries to the collection
+        countries.setdefault(player['country'], {})
+        # iterate through each country and check it against
+        # each country that the player is associated with
+        # if there's a match add that players position to the
+        # country, and passing an empty array as the default
+        for country in countries:
+            if (player['country'] == country):
+                countries[country].setdefault(player['position'], [])
+                countries[country][player['position']].append(player)
+
+    return countries
