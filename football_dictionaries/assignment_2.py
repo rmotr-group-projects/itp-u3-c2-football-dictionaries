42ba55a8f7270aa053d5697567d1bbfb4dc48a5a
Andrew McDonald <elderalm@yahoo.com>
2017-10-25 14:53:56 +0000

    uploading assignments 1-3

--- football_dictionaries/assignment_2.py
+++ football_dictionaries/assignment_2.py
@@ -1,2 +1,12 @@
-def players_by_position(squads_list):
-    pass
+from assignment_1 import players_as_dictionaries
+
+def players_by_position(squad_list):
+    player_list = players_as_dictionaries(squad_list)
+    new_player_dictionary = {}
+    for player in player_list:
+        if player['position'] not in new_player_dictionary:
+            new_player_dictionary[player['position']] = []
+        new_player_dictionary[player['position']].append(player)
+    return new_player_dictionary
+        
+
--- football_dictionaries/assignment_3.py
+++ football_dictionaries/assignment_3.py
@@ -1,2 +1,13 @@
-def players_by_country_and_position(squads_list):
-    pass
+from assignment_1 import players_as_dictionaries
+
+
+def players_by_country_and_position(squad_list):
+    player_list = players_as_dictionaries(squad_list)
+    new_player_dictionary = {}
+    for player in player_list:
+        if player['country'] not in new_player_dictionary:
+            new_player_dictionary[player['country']] = {}
+        if player['position'] not in new_player_dictionary[player['country']]:
+            new_player_dictionary[player['country']][player['position']] = []
+        new_player_dictionary[player['country']][player['position']].append(player)
+    return new_player_dictionary
