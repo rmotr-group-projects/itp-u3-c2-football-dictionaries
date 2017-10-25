d9dee90a703e0b526cc20e1bf63ddc9110c7fc01
Dan Tembe <31318833+dtembe@users.noreply.github.com>
2017-10-25 09:58:43 -0500

    Fixed assingment 3. As per Santiago's class instruction. Should have all 3 working now.

--- football_dictionaries/assignment_3.py
+++ football_dictionaries/assignment_3.py
@@ -1,10 +1,28 @@
+from squads_data import SQUADS_DATA
+
+
+def player_list_to_dict(player_list):
+    return{'number': player_list[0],
+           'position': player_list[1],
+           'name': player_list[2],
+           'date_of_birth': player_list[3],
+           'caps': player_list[4],
+           'club': player_list[5],
+           'country': player_list[6],
+           'club_country': player_list[7],
+           'year': player_list[8]
+           }
+
+
 def players_by_country_and_position(squads_list):
-    pass
-
-
-
-# Test function locally
-from football_dictionaries.squads_data import SQUADS_DATA
-squads_list = SQUADS_DATA
-testcode = players_by_country_and_position(squads_list)
-print(testcode)
+    result = {}
+    for player_list in squads_list:
+        player = player_list_to_dict(player_list)
+        country = player['country']
+        position = player['position']
+        if country not in result:
+            result[country] = {}
+        if position not in result[country]:
+            result[country][position] = []
+        result[country][position].append(player)
+    return result
