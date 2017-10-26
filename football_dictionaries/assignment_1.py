3f377ce3ff250d155bc371accdd0a70b7be63898
Michael S <michael_j_smythe@yahoo.com>
2017-10-26 21:43:43 +0000

    solutions for project3

--- football_dictionaries/assignment_1.py
+++ football_dictionaries/assignment_1.py
@@ -1,2 +1,9 @@
 def players_as_dictionaries(squads_list):
-    pass
+    dict_master = []
+    dict_order = ["number", "position", "name", "date_of_birth", "caps","club","country","club_country","year"]
+    for player in squads_list:
+        temp_dict = {}
+        for value,key in zip(player, dict_order):
+            temp_dict.update({key:value})
+        dict_master.append(temp_dict)
+    return dict_master
