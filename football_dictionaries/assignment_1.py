1aba5e79b0788c0fac6016e94b888b381521da7d
Amy McCarther <amy.mccarther@yahoo.com>
2017-10-26 20:52:52 +0000

    Project_3

--- football_dictionaries/assignment_1.py
+++ football_dictionaries/assignment_1.py
@@ -1,2 +1,12 @@
+from pprint import pprint
+from squads_data import SQUADS_DATA
+
 def players_as_dictionaries(squads_list):
-    pass
+    list_key = ['number', 'position', 'name', 'date_of_birth', 'caps', 'club', 'country', 'club_country', 'year']
+    results = []
+    for item in squads_list:
+        results.append(dict(zip(list_key,item)))
+    return results
+
+    
+#pprint(players_as_dictionaries(SQUADS_DATA))
--- football_dictionaries/assignment_2.py
+++ football_dictionaries/assignment_2.py
@@ -1,2 +1,17 @@
+from pprint import pprint
+from squads_data import SQUADS_DATA
+from assignment_1 import players_as_dictionaries
+
 def players_by_position(squads_list):
-    pass
+    squad = players_as_dictionaries(squads_list)
+    by_position = {}
+    
+    for player in squad:
+        position = player['position']
+        by_position.setdefault(position, [])
+        by_position[position].append(player)
+    
+    return by_position
+
+
+#pprint(players_by_position(SQUADS_DATA))
