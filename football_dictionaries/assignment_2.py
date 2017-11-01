b6df7d1cda04033187f10ac3636fd3430ae02996
Lexie <maionhigh@gmail.com>
2017-11-01 17:01:14 +0000

    Added the football dictionaries

--- football_dictionaries/assignment_2.py
+++ football_dictionaries/assignment_2.py
@@ -1,2 +1,17 @@
+from .assignment_1 import players_as_dictionaries
+
 def players_by_position(squads_list):
-    pass
+    players_in_pos_dict = {}
+    
+    players_in_dict_list = players_as_dictionaries(squads_list)
+    
+    for player in players_in_dict_list:
+        position = player["position"]
+        
+        if players_in_pos_dict.get(position) == None:
+            players_list = [player]
+            players_in_pos_dict[position] = players_list
+        else:
+            players_in_pos_dict[position].append(player)
+    
+    return players_in_pos_dict
