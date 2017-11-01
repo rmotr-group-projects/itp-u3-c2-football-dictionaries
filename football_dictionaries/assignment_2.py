aa0ab8952d5be0e1d267632ee5f081bb146cb8e6
yeli-buonya <yeli.e.buonya@gmail.com>
2017-11-01 15:42:08 -0700

    refactor

--- football_dictionaries/assignment_2.py
+++ football_dictionaries/assignment_2.py
@@ -2,20 +2,17 @@ from football_dictionaries.assignment_1 import players_as_dictionaries
 
 
 def players_by_position(squads_list):
-    positions = []
-    positions_list = {}
     formatted_list = players_as_dictionaries(squads_list)
-
+    positions_list = {}
+    
     # gets positions from all the players
     for player in formatted_list:
-        player_position = player.get('position')
-        positions.append(player_position)
-        # sets default positions for positions list
-        for position in positions:
-            positions_list.setdefault(position, [])
-        # if position matches, add the player to that group            
-        for position in positions_list:
-            if player_position == position:
-                positions_list[player_position].append(player)
+        position = player.get('position')
+        # adds a field position
+        positions_list.setdefault(position, [])
+        # adds the player to their respective list
+        # by position
+        positions_list[position].append(player)
 
     return positions_list
+ 
