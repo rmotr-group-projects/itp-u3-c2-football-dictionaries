9b619ce4aad04e2c38a8a7a5c3a0c2db3ff28979
Milana <ventusnocturnus@gmail.com>
2018-02-25 02:09:10 +0000

    updated project code

--- football_dictionaries/assignment_2.py
+++ football_dictionaries/assignment_2.py
@@ -1,2 +1,15 @@
+from football_dictionaries.assignment_1 import players_as_dictionaries
+
 def players_by_position(squads_list):
-    pass
+    players_list = players_as_dictionaries(squads_list)
+    
+    result={}
+    
+    for player in players_list:
+        position=player['position']
+        
+        result.setdefault(position,[])
+        result[position].append(player)
+    
+    return result
+    
