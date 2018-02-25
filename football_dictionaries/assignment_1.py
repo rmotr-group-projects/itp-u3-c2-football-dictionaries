4ed8ed1cf15fcef92bfe938ba19113c0a30382c3
Eric Garcia <eggarcia2891@gmail.com>
2018-02-25 04:11:28 +0000

    initial pass

--- football_dictionaries/assignment_1.py
+++ football_dictionaries/assignment_1.py
@@ -1,2 +1,18 @@
 def players_as_dictionaries(squads_list):
-    pass
+    
+    result = [] 
+    
+    for player in squads_list:
+        player_dict = {}
+        player_dict['number'] = player[0]
+        player_dict['position'] = player[1]
+        player_dict['name'] = player[2]
+        player_dict['date_of_birth'] = player[3]
+        player_dict['caps'] = player[4]
+        player_dict['club'] = player[-4]
+        player_dict['country'] = player[-3]
+        player_dict['club_country'] = player[-2]
+        player_dict['year'] = player[-1]
+        result.append(player_dict)
+    
+    return result
--- football_dictionaries/assignment_3.py
+++ football_dictionaries/assignment_3.py
@@ -1,2 +1,13 @@
+from football_dictionaries.assignment_1 import players_as_dictionaries
+
 def players_by_country_and_position(squads_list):
-    pass
+    
+    country_dict = {}
+    player_list = players_as_dictionaries(squads_list) #this is a list
+    
+    for player in player_list: #creates an empty dictionary for each footballing country  
+        country_dict.setdefault(player['country'], {})
+        country_dict[player['country']].setdefault(player['position'], []) #ex: {"Argentina: []"}
+        country_dict[player['country']][player['position']].append(player) #adds player into the proper position in their country
+    
+    return country_dict
