c6cdeadfe8f0aa7cd140dda0ce5efb0eaaa45ccd
Mario  Rodriguez <useraddmario@gmail.com>
2018-02-26 03:01:40 +0000

    football_dictionaries complete

--- football_dictionaries/assignment_1.py
+++ football_dictionaries/assignment_1.py
@@ -1,2 +1,19 @@
+#from squads_data import SQUADS_DATA
+
 def players_as_dictionaries(squads_list):
-    pass
+    #iterate over input while creating generic dictionaries from set(keys_list) values
+    #fill dict(squad) with the input from player in squads_list
+    #append to list(players) 
+    #return list(players)
+    players = []
+    keys_list = ['number', 'position', 'name', 'date_of_birth', 'caps', 'club', 'country', 'club_country', 'year']
+    
+    for player in squads_list:
+            squad = {}
+            index = 0
+            squad = dict.fromkeys(set(keys_list))
+            for stat in player:
+                squad[keys_list[index]] = player[index]
+                index += 1
+            players.append(squad)
+    return players   
--- football_dictionaries/assignment_3.py
+++ football_dictionaries/assignment_3.py
@@ -1,2 +1,64 @@
+from squads_data import SQUADS_DATA
+from assignment_2 import players_by_position
+
+
+
 def players_by_country_and_position(squads_list):
-    pass
+    positions = []
+    countries = []
+    result = {
+     'Belgium': {},
+     'Brazil': {},
+     'Argentina': {},
+     'South Korea': {},   
+      }
+    #create list(countries) with possible countries
+    for player in squads_list:
+        country = player[-3]
+        if country not in countries:
+            countries.append(country)
+    #create list(positions) with possible positions
+    for player in squads_list:
+        position = player[1]
+        if position not in positions:
+            positions.append(position)
+
+
+    argentina = []
+    belgium = []
+    brazil = []
+    skorea = []
+    #separate players by country with player[-3] from input
+    for player in squads_list:
+        if player[-3] == 'Argentina':
+            argentina.append(player)
+        elif player[-3] == 'Belgium':
+            belgium.append(player)
+        elif player[-3] == 'Brazil':
+            brazil.append(player)
+        else:
+            skorea.append(player)
+
+    #use players_by_position to separate each countries' players by position
+    sun_of_may = players_by_position(argentina)
+    black_lion = players_by_position(belgium)
+    ordem_e_progresso = players_by_position(brazil)
+    taegukgi = players_by_position(skorea)
+
+
+    #update the first half with the second
+    result['Argentina'].update(sun_of_may)
+    result['Belgium'].update(black_lion)
+    result['Brazil'].update(ordem_e_progresso)
+    result['South Korea'].update(taegukgi)
+
+    #iterate through countries and positions to find and del any result[country][position] == []
+    for country in countries:
+        #print(country)
+        for position in positions:
+            #print(position)
+            if result[country][position] == []:
+                del result[country][position]
+                
+    #Miller time
+    return result
