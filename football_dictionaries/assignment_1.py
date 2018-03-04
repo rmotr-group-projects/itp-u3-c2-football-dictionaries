6922425229471fccd1529bfcb2ca4d09bb8e4036
PQ1 <philipquach@outlook.com>
2018-03-04 19:42:57 +0000

    Completed Assignment and passed tests

--- football_dictionaries/assignment_1.py
+++ football_dictionaries/assignment_1.py
@@ -1,2 +1,34 @@
-def players_as_dictionaries(squads_list):
-    pass
+from squads_data import SQUADS_DATA
+
+# Transform 1 record of list to dictionary function
+def transform_dict(player):
+   # dictionary that assign keys to position in list from parameter
+    result = {'caps': player[4],
+        'club': player[5],
+        'club_country': player[7],
+        'country': player[6],
+        'date_of_birth': player[3],
+        'name': player[2],
+        'number': player[0],
+        'position': player[1],
+        'year': player[8]
+    }
+    
+    return result
+
+
+def players_as_dictionaries(squad_list):
+    # Empty list to hold all dictionaries
+    player_list = []
+    
+    # Loop through parameter list
+    for player in squad_list:
+        # Pass the current position from list into function to transfer to dict
+        player = transform_dict(player)
+        # Append entire dict to list
+        player_list.append(player)
+    
+    return player_list
+
+
+#print(players_as_dictionaries(SQUADS_DATA))
--- main.py
+++ main.py
@@ -1,7 +1,13 @@
+# From the football_dictionaries dir look at squads_data.py that has the squad data in a list
 from football_dictionaries.squads_data import SQUADS_DATA
+# From the football_dictionaries dir look at assignment_1.py import the function players_as_dictionaries
 from football_dictionaries.assignment_1 import players_as_dictionaries
+from football_dictionaries.assignment_2 import players_by_position
+
 from pprint import pprint
 
 
-result = players_as_dictionaries(SQUADS_DATA)
-pprint(result)
+#result = players_as_dictionaries(SQUADS_DATA)
+result2 = players_by_position(SQUADS_DATA)
+#pprint(result)
+pprint(result2)
