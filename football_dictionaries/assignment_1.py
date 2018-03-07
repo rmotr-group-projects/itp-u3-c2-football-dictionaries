0da28d7d1506162bddfaab0d86e48e577ca640d2
Berenice Agustina Larsen Pereyra <berelarsenp@gmail.com>
2018-03-07 20:11:39 +0000

    Football project working!

--- football_dictionaries/assignment_1.py
+++ football_dictionaries/assignment_1.py
@@ -1,2 +1,20 @@
+def transform_list_to_dict(player_as_list):
+    player_dict = {
+        'number': player_as_list[0],
+        'position': player_as_list[1],
+        'name': player_as_list[2],
+        'date_of_birth': player_as_list[3],
+        'caps': player_as_list[4],
+        'club': player_as_list[5],
+        'country': player_as_list[6],
+        'club_country': player_as_list[7],
+        'year': player_as_list[8],
+    }
+    return player_dict
+
 def players_as_dictionaries(squads_list):
-    pass
+    list_of_dicts = []
+    for player in squads_list:
+        player_as_dict = transform_list_to_dict(player)
+        list_of_dicts.append(player_as_dict)
+    return list_of_dicts
--- football_dictionaries/assignment_3.py
+++ football_dictionaries/assignment_3.py
@@ -1,2 +1,12 @@
+from .assignment_1 import players_as_dictionaries
+
 def players_by_country_and_position(squads_list):
-    pass
+    player_as_dict = players_as_dictionaries(squads_list)
+    dict_by_country = {}
+    for player in player_as_dict:
+        country = player['country']
+        dict_by_country.setdefault(country, {})
+        position = player['position']
+        dict_by_country[country].setdefault(position, [])
+        dict_by_country[country][position].append(player)
+    return dict_by_country
