881a43564a0518ed1c68833238e4b886d5019209
cmcs0157 <cmcs0157@gmail.com>
2018-03-26 04:59:32 +0000

    Assignment 1 Work in Progress

--- football_dictionaries/assignment_1.py
+++ football_dictionaries/assignment_1.py
@@ -1,4 +1,24 @@
+from squads_data import SQUADS_DATA as squads_list
+
 def players_as_dictionaries(squads_list):
     new_squads_list = squads_list[:]
-    print(squads_list)
+    player_dict_format = []
+    for player in new_squads_list:
+        player_bio = {
+            'number': None,
+            'position': None,
+            'name': None,
+            'date_of_birth': None,
+            'caps': None,
+            'club': None,
+            'country': None,
+            'club_country': None,
+            'year': None
+        }
+        for bio_info in player:
+            player_bio[key] = bio_info
+        player_dict_format.append(player_bio)
+    return player_dict_format
+    
+players_as_dictionaries(squads_list)
     
