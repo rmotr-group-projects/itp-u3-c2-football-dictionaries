576a5e556283744428bcd7d105a5a3240444a5b6
cmcs0157 <cmcs0157@gmail.com>
2018-03-27 19:29:39 +0000

    Assignment 1 Rough Draft 2

--- football_dictionaries/assignment_1.py
+++ football_dictionaries/assignment_1.py
@@ -1,10 +1,5 @@
-from squads_data import SQUADS_DATA as squads_list
-
-new_squads_list = squads_list[:]
-
-
-def players_as_dictionaries(new_squads_list):
-    # This function returns info of players of a squad into a dictionary
+def players_as_dictionaries(squads_list):
+    # Returns info of players of a squad into a dictionary
     overall_squad_bio = []
     player_bio_info = [
         'number',
@@ -16,9 +11,9 @@ def players_as_dictionaries(new_squads_list):
         'country',
         'club_country',
         'year'
-]
-    for player in new_squads_list:
-        """Uses the zip method to combine 'player_bio_info' with 'player' info list 
+    ]
+    for player in squads_list:
+        """Uses the zip function to combine player_bio_info with player info lists 
            and then turns it into a dictionary"""
         combined_lists_dict = dict(zip(player_bio_info, player))
         overall_squad_bio.append(combined_lists_dict)
