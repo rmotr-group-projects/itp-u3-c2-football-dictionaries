2b399e0fca46e3da4f70fe8a6eb7352ccb2265cf
Andrew DeNike <hawkra@aol.com>
2018-04-04 18:22:13 +0000

    Completed Football Dictionaries w/ comments

--- football_dictionaries/assignment_2.py
+++ football_dictionaries/assignment_2.py
@@ -1,2 +1,12 @@
+from assignment_1 import players_as_dictionaries
+from squads_data import SQUADS_DATA
+
 def players_by_position(squads_list):
-    pass
+    position_dict = {}  #dictionary with positions as keys and players that play those positions as values
+    players_list = players_as_dictionaries(squads_list)  #using players_as_dictionaries(SQUADS_DATA) to access players dictionary
+    for player in players_list: 
+        position_dict.setdefault(player['position'], [])  #initializing the players position with an empty list within position_dict if it doesnt exist
+        position_dict[player['position']].append(player)  #appending player to the player's respective position key within position_dict
+    return position_dict
+
+print(players_by_position(SQUADS_DATA))
--- football_dictionaries/assignment_3.py
+++ football_dictionaries/assignment_3.py
@@ -1,2 +1,11 @@
+from football_dictionaries.assignment_1 import players_as_dictionaries
+from football_dictionaries.squads_data import SQUADS_DATA
+
 def players_by_country_and_position(squads_list):
-    pass
+    players_country_dict = {}  #dictionary with countries as keys and values that are dictionaries with position as key and lists of each player as values 
+    players_list = players_as_dictionaries(squads_list)  #using players_as_dictionaries(SQUADS_DATA) to access players dictionary
+    for player in players_list:
+        players_country_dict.setdefault(player['country'], {})  #initialzing players country as keys with values of empty dictionaries
+        players_country_dict[player['country']].setdefault(player['position'], [])  #accessing the country value of the country key and initialzing position as key with value of empty list if it doesnt exist
+        players_country_dict[player['country']][player['position']].append(player)  #accesing the country value, a dictionary with keys of positions and then accessing the values within this dictionary and appending the player to list
+    return players_country_dict
