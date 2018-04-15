92eb20bab22d1b186158ff5e1b813c5b1520659a
Taiwei Ko <mike25ko@gmail.com>
2018-04-15 14:30:43 -0600

    Finished Football Dictioinaries Project

--- football_dictionaries/assignment_2.py
+++ football_dictionaries/assignment_2.py
@@ -1,2 +1,23 @@
+from football_dictionaries.assignment_1 import players_by_dictionaries
+
 def players_by_position(squads_list):
-    pass
+    
+    position_list = set()
+
+    for players in squads_list:
+    	position_list.add(players[1])
+
+    position_dict = {}
+
+    for position in position_list:
+
+    	positions = []
+
+    	for players in squads_list:
+    		if position in players:
+    			positions.append(players)
+
+    	position_dict[position] = players_by_dictionaries(positions)
+
+    return position_dict
+
