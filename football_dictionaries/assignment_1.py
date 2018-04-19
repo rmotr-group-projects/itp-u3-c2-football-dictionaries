f1b34263a10760e2c35cc3de1096c255fcff7cf6
renjuzac <renjuzac@gmail.com>
2018-04-19 04:21:54 +0000

    Assignment solutions

--- football_dictionaries/assignment_1.py
+++ football_dictionaries/assignment_1.py
@@ -1,2 +1,20 @@
 def players_as_dictionaries(squads_list):
-    pass
+    result = []
+    
+    keys = ['number',
+    'position',
+    'name',
+    'date_of_birth',
+    'caps',
+    'club',
+    'country',
+    'club_country',
+    'year']
+    
+    for squad in squads_list:
+        item_dict ={}
+        for key,item in zip(keys,squad):
+            item_dict[key] = item
+        result.append(item_dict)
+    
+    return result
