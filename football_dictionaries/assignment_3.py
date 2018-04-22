0bbdea0f62a5d510fc1649396683add8b2ebe285
Shanna <horsecrazygirl42@yahoo.com>
2018-04-22 03:31:16 +0000

    Solved Assignment 3

--- football_dictionaries/assignment_3.py
+++ football_dictionaries/assignment_3.py
@@ -1,2 +1,29 @@
+from football_dictionaries.assignment_2 import players_by_position
+
 def players_by_country_and_position(squads_list):
-    pass
+    
+    players = players_by_position(squads_list)
+    
+    result = {
+        'Argentina': {
+            'GK' : [],
+            'FW' : []
+        },
+        'Belgium': {
+            'GK' : [],
+            'MF' : []
+        },
+        'Brazil': {
+            'MF' : []
+        },
+        'South Korea': {
+            'FW' : []
+        }
+    }
+    
+    for position in players:
+        for player in players[position]:
+            result[player['country']][player['position']].append(player)
+    
+    return result
+    
