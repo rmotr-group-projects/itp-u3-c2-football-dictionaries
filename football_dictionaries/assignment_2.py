edecb018f628652cf798f0fe02e38e403fec8283
Tae Yu <taeyu7720@gmail.com>
2018-04-25 22:02:54 +0000

    First and Final

--- football_dictionaries/assignment_2.py
+++ football_dictionaries/assignment_2.py
@@ -1,2 +1,20 @@
+from .assignment_1 import players_as_dictionaries
+
 def players_by_position(squads_list):
-    pass
+    
+    positions_dict = {}
+    
+    for player in players_as_dictionaries(squads_list):
+        
+        position = player['position']
+        
+        # if position not in positions_dict:
+        #     positions_dict[position] = []
+        
+        positions_dict.setdefault(position, [])
+            
+        positions_dict[position].append(player)
+            
+    return positions_dict
+
+
