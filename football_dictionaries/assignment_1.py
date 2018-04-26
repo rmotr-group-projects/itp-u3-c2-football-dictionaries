94670e539173340438bded1972c471d42908348c
Ben Hedgepeth <airmaxmoney@protonmail.com>
2018-04-26 00:15:15 +0000

    Assignments 1-3 added

--- football_dictionaries/assignment_1.py
+++ football_dictionaries/assignment_1.py
@@ -1,2 +1,24 @@
+from football_dictionaries.squads_data import SQUADS_DATA
+import collections
+
 def players_as_dictionaries(squads_list):
-    pass
+    '''Return a collection of football player information'''
+    
+    team_lineup = []
+    team_info = ['number', 'position', 'name', 'date_of_birth', 'caps', 'club', 'country', 'club_country', 'year']
+   
+    for data_set in squads_list:
+        player_bio = collections.OrderedDict()
+    
+        for data_index, data in enumerate(data_set):
+            player_bio[team_info[data_index]] = data
+        
+        team_lineup.append(player_bio)
+    
+    return team_lineup
+    
+
+print(players_as_dictionaries(SQUADS_DATA))
+    
+
+
--- football_dictionaries/assignment_3.py
+++ football_dictionaries/assignment_3.py
@@ -1,2 +1,16 @@
+from football_dictionaries.assignment_1 import players_as_dictionaries
+
 def players_by_country_and_position(squads_list):
-    pass
+    '''Organize football players by their home country and their position'''
+    
+    player_background = {}
+    players = players_as_dictionaries(squads_list)
+    for player in players:
+        country = player['country']
+        position = player['position']
+        player_background.setdefault(country, {})
+        player_background[country].setdefault(position, [])
+        
+        player_background[country][position].append(player)
+        
+    return player_background    
