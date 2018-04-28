6914424e95b28529ad7a2f8740ccd28d00bacf3a
Arjunan Gnanendran <arjun.gnanendran@gmail.com>
2018-04-28 01:51:38 +0000

    football assignment

--- football_dictionaries/assignment_2.py
+++ football_dictionaries/assignment_2.py
@@ -1,2 +1,30 @@
+#import pprint as pp
+#from squads_data import SQUADS_DATA
+
 def players_by_position(squads_list):
-    pass
+    players = []
+    attributes = [
+        'number', 
+        'position', 
+        'name', 
+        'date_of_birth', 
+        'caps', 
+        'club', 
+        'country', 
+        'club_country', 
+        'year'
+    ]
+    
+    for player in squads_list:
+        player_dict = {}
+        for i in range(len(player)):
+            player_dict[attributes[i]] = player[i]
+        players.append((player_dict))
+    
+    positions = {}
+    for player in players:
+        positions.setdefault(player['position'], [])
+        positions[player['position']].append(player)
+    return positions
+    
+#pp.pprint(players_by_position(SQUADS_DATA))
--- football_dictionaries/assignment_3.py
+++ football_dictionaries/assignment_3.py
@@ -1,2 +1,32 @@
+#import pprint as pp
+#from squads_data import SQUADS_DATA
+
 def players_by_country_and_position(squads_list):
-    pass
+    players = []
+    attributes = [
+        'number', 
+        'position', 
+        'name', 
+        'date_of_birth', 
+        'caps', 
+        'club', 
+        'country', 
+        'club_country', 
+        'year'
+    ]
+    
+    for player in squads_list:
+        player_dict = {}
+        for i in range(len(player)):
+            player_dict[attributes[i]] = player[i]
+        players.append((player_dict))
+    
+    countries = {}
+    for player in players:
+        countries.setdefault(player['country'],{})
+        countries[player['country']].setdefault(player['position'], [])
+        countries[player['country']][player['position']].append(player)
+    return countries
+    
+#pp.pprint(players_by_country_and_position(SQUADS_DATA))
+
