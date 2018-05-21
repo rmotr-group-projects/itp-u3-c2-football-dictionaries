4ff58937f08876808384a2bd58cc84061b6444cf
kufopo <kufopo@gmail.com>
2018-05-21 19:35:37 +0000

    Assignment fixed. Took the whole weekend.

--- football_dictionaries/assignment_1.py
+++ football_dictionaries/assignment_1.py
@@ -1,2 +1,15 @@
 def players_as_dictionaries(squads_list):
-    pass
+   
+    keys =  ['number', 'position','name','date_of_birth','caps','club',"country",'club_country','year']
+    squad = []
+    
+    for player in squads_list:
+        newdict ={}
+        for i in range(0,len(keys)):
+            newdict[keys[i]]=player[i]
+        squad.append(newdict)
+            
+                
+    return squad
+            
+        
--- football_dictionaries/assignment_3.py
+++ football_dictionaries/assignment_3.py
@@ -1,2 +1,50 @@
 def players_by_country_and_position(squads_list):
-    pass
+    postions = ['GK', 'DF', 'MF', 'FW']
+    keys = ['number', 'position', 'name', 'date_of_birth', 'caps', 'club', "country", 'club_country', 'year']
+    #mega = []
+
+    stage = []
+    county = []
+
+
+
+    for player in squads_list:
+        newd = {}
+
+        for j in range(0, len(postions)):
+
+
+            if player[1] == postions[j]:
+                for w in range(0,len(keys)):
+                    newd[keys[w]]=player[w]
+
+                stage.append(newd)
+
+
+#getting the couuntries into county variable
+    for item  in stage:
+        if item['country'] not in county:
+            county.append(item['country'])
+
+    grandmummy = {}
+
+    for country in county:
+
+        hurricane = {}
+
+        for  q in postions:
+            storm = []
+
+            for v in stage:
+
+                if v['position']==q and v['country']==country:
+
+                    storm.append(v)
+                    hurricane[q]=storm
+                    grandmummy[country]=hurricane
+
+
+
+
+    return grandmummy
+
