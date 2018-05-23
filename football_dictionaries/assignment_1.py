e090b57483065d9b9b79d90a4d646c6be6ff7e39
Peter Ryan <peter.e.ryan@gmail.com>
2018-05-23 23:31:52 +0000

    Football Project - Peter Ryan

--- football_dictionaries/assignment_1.py
+++ football_dictionaries/assignment_1.py
@@ -1,2 +1,24 @@
 def players_as_dictionaries(squads_list):
-    pass
+    new_squads_list = []
+    
+    for squad in squads_list:
+        squads_dict = {}
+        squads_dict['caps'] = squad[4]
+        squads_dict['club'] = squad[5]
+        squads_dict['club_country'] = squad[7]
+        squads_dict['country'] = squad[6]
+        squads_dict['date_of_birth'] = squad[3]
+        squads_dict['name'] = squad[2]
+        squads_dict['number'] = squad[0]
+        squads_dict['position'] = squad[1]
+        squads_dict['year'] = squad[8]
+        new_squads_list.append(squads_dict)
+        
+    return new_squads_list
+
+
+
+     
+
+
+
--- football_dictionaries/assignment_3.py
+++ football_dictionaries/assignment_3.py
@@ -1,2 +1,25 @@
+from football_dictionaries.assignment_1 import players_as_dictionaries
+
 def players_by_country_and_position(squads_list):
-    pass
+    squads_dict = players_as_dictionaries(squads_list)
+    
+    players_by_country_and_positions_dict = {}
+    
+    for squad in squads_dict:
+        country = squad['country']
+        players_by_country_and_positions_dict.setdefault(country, {})
+        
+    for country in players_by_country_and_positions_dict:
+        players_by_position_dict = {}
+        
+        for squad in squads_dict:
+            if country == squad['country']:
+                if squad['position'] not in players_by_position_dict:
+                    players_by_position_dict[squad['position']] = [squad] 
+                else:
+                    players_by_position_dict[squad['position']].append(squad)
+        players_by_country_and_positions_dict[country] = players_by_position_dict
+    
+    return players_by_country_and_positions_dict
+ 
+ 
