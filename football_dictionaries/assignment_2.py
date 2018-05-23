69bfa3c41af717111701514789973f8f58ecd708
Dylan Cruse <dylancruse@live.com>
2018-05-23 01:02:49 +0000

    All tests passed

--- football_dictionaries/assignment_2.py
+++ football_dictionaries/assignment_2.py
@@ -1,2 +1,19 @@
+from football_dictionaries.assignment_1 import players_as_dictionaries
+
 def players_by_position(squads_list):
-    pass
+    players_dictionaries = players_as_dictionaries(squads_list)
+    
+    result = {}
+    
+    for player in players_dictionaries:
+        position = player['position']
+        result.setdefault(position, [])
+        result[position].append(player)
+        
+        
+    
+    
+    
+    
+    
+    return result
