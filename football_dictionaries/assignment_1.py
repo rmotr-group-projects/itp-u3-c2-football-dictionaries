f701d62d740f1e92324f1b1718a44aeaa5e0df67
Brian Beegan <brianbeegan@yahoo.com>
2018-06-23 22:07:19 +0000

    hope this works

--- football_dictionaries/assignment_1.py
+++ football_dictionaries/assignment_1.py
@@ -1,2 +1,20 @@
-def players_as_dictionaries(squads_list):
-    pass
+def players_as_dictionaries(sqauds_data):
+    player_dict_list = []
+    playerlist = []
+    newlist = []
+    count = 0 
+    for playerinfo in sqauds_data:
+        playerlist+=[playerinfo]
+        player_dict_list+=[{
+            'number': playerlist[count][0],
+            'position': playerlist[count][1],
+            'name': playerlist[count][2],
+            'date_of_birth': playerlist[count][3],
+            'caps': playerlist[count][4],
+            'club': playerlist[count][5],
+            'country': playerlist[count][6],
+            'club_country': playerlist[count][7],
+            'year': playerlist[count][8],
+             }]
+        count+=1
+    return(player_dict_list)
