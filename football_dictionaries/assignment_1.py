7c9b755fc1568feb0894f4a8b8dfbd07363946bc
Debora Galeano <debygalser+c9@gmail.com>
2018-06-23 17:31:31 +0000

    Football Dict Assignment

--- football_dictionaries/assignment_1.py
+++ football_dictionaries/assignment_1.py
@@ -1,2 +1,17 @@
 def players_as_dictionaries(squads_list):
-    pass
+    items = ['number','position','name', 'date_of_birth', 'caps', 'club', 'country', 'club_country', 'year']
+    # items[0], items[1], items[2], items[3]
+    result = []
+
+    for k in squads_list: # k is each list
+        e_dict= {}
+        count = 0
+        for value in k: # value is each item in k
+            # e_dict[key] = value ==> {'number': value}
+            e_dict[items[count]] = value
+            count += 1
+        result.append(e_dict) 
+ 
+    return result
+    
+
--- football_dictionaries/assignment_2.py
+++ football_dictionaries/assignment_2.py
@@ -1,2 +1,15 @@
+from football_dictionaries.assignment_1 import players_as_dictionaries
+from football_dictionaries.squads_data import SQUADS_DATA
+
 def players_by_position(squads_list):
-    pass
+    result = players_as_dictionaries(squads_list)
+    
+    positions= {}
+
+    for player in result:
+        position_key = player['position']
+        positions.setdefault(position_key , []) # stores the new info in new list
+        positions[position_key].append(player)  
+    
+    return positions
+
