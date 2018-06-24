009b4fc17a5fb6bce4bdbd3cbeb7e0943cf743bf
Craig Rupp <craigrupp@utexas.edu>
2018-06-24 22:02:52 +0000

    6/24 3 assignment initial push

--- football_dictionaries/assignment_2.py
+++ football_dictionaries/assignment_2.py
@@ -1,2 +1,37 @@
+# # Please note we're returning a dictionary instead of a list
+# {
+#   "GK": [{..player1..}, {..player2..}],
+#   "DF": [{..player1..}, {..player2..}],
+#   "MF": [{..player1..}, {..player2..}],
+#   "FW": [{..player1..}, {..player2..}],
+# }
+from squads_data import SQUADS_DATA
 def players_by_position(squads_list):
-    pass
+    result = {
+        'GK' : [],
+        'DF' : [],
+        'MF' : [],
+        'FW' : []
+    }
+    for player in squads_list:
+        temp_obj = {
+            'number'        : player[0],
+            'position'      : player[1],
+            'name'          : player[2],
+            'date_of_birth' : player[3],
+            'caps'          : player[4],
+            'club'          : player[-4],
+            'country'       : player[-3],
+            'club_country'  : player[-2],
+            'year'          : player[-1]
+        }
+        if temp_obj['position'] == 'GK':
+            result['GK'].append(temp_obj)
+        elif temp_obj['position'] == 'DF':
+            result['DF'].append(temp_obj)
+        elif temp_obj['position'] == 'MF':
+            result['MF'].append(temp_obj)
+        else:
+            result['FW'].append(temp_obj)
+    return result
+print(players_by_position(SQUADS_DATA))
--- football_dictionaries/assignment_3.py
+++ football_dictionaries/assignment_3.py
@@ -1,2 +1,45 @@
+# {
+#   "Argentina": {
+#     "GK": [{..player1..}, {..player2..}],
+#     "DF": [{..player1..}, {..player2..}],
+#     "MF": [{..player1..}, {..player2..}],
+#     "FW": [{..player1..}, {..player2..}],
+#   },
+#   "Brazil": {
+#     "GK": [{..player1..}, {..player2..}],
+#     "DF": [{..player1..}, {..player2..}],
+#     "MF": [{..player1..}, {..player2..}],
+#     "FW": [{..player1..}, {..player2..}],
+#   }
+# }
+from squads_data import SQUADS_DATA
+from pprint import pprint 
 def players_by_country_and_position(squads_list):
-    pass
+    result = {}
+    for player in squads_list:
+        temp_obj = {
+            'number'        : player[0],
+            'position'      : player[1],
+            'name'          : player[2],
+            'date_of_birth' : player[3],
+            'caps'          : player[4],
+            'club'          : player[-4],
+            'country'       : player[-3],
+            'club_country'  : player[-2],
+            'year'          : player[-1]
+        }
+        key_check = temp_obj['country']
+        position_check = temp_obj['position']
+        if key_check in result:
+            if position_check in result[key_check]:
+                result[key_check][position_check].append(temp_obj)
+            else:
+                result[key_check][position_check] = []
+                result[key_check][position_check].append(temp_obj) 
+        else:
+            result[key_check] = {}
+            result[key_check][temp_obj['position']] = []
+            result[key_check][temp_obj['position']].append(temp_obj)
+    return result
+pprint(players_by_country_and_position(SQUADS_DATA))
+        
