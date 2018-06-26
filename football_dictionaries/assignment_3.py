1b58499343f4ef24913b7707b8f17368c50be5ae
Marcos Vivanco <marcosviv88@gmail.com>
2018-06-26 00:29:04 +0000

    Project working!

--- football_dictionaries/assignment_3.py
+++ football_dictionaries/assignment_3.py
@@ -1,2 +1,21 @@
+from pprint import pprint
+from .squads_data import SQUADS_DATA as squad;
+from .assignment_1 import players_as_dictionaries
+
 def players_by_country_and_position(squads_list):
-    pass
+    # create containers outside the loop which will be filled with values from inside the loop
+    countries = dict()
+    positions = dict()
+    
+    # create the list of players as dictionaries - we've already done this in assignment 1
+    players = players_as_dictionaries(squads_list)
+    
+    # loop over every player in the unsorted dictionary
+    for player in players: 
+        # for each player's country, create a dictionary with keys for each value if it does not exist with an empty dictionary as the value
+        countries.setdefault(player['country'], {} )
+        # for each of the countries we just created, fill the empty dictionary with each position if it does not exist with an empty list as the value
+        countries[player['country']].setdefault(player['position'], [] )
+        # when player (from for player in players) matches the 'country' and 'position' keys created, add that player to the list
+        countries[player['country']][player['position']].append(player)
+    return countries
