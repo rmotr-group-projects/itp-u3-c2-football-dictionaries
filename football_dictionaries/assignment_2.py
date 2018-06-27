d5cf58e1c0fe8d8b6ce795cc5ea07f7051f684be
Forrest MacDonald <forrest.macdonald@gmail.com>
2018-06-27 20:43:42 +0000

    final version trying again

--- football_dictionaries/assignment_2.py
+++ football_dictionaries/assignment_2.py
@@ -1,2 +1,13 @@
+from pprint import pprint
+
+from .assignment_1 import players_as_dictionaries
+from .squads_data import SQUADS_DATA
+
+
 def players_by_position(squads_list):
-    pass
+    result_dict = players_as_dictionaries(squads_list)
+    result = {}
+    for player in result_dict:
+        result.setdefault(player['position'], [])
+        result[player['position']].append(player)
+    return result
