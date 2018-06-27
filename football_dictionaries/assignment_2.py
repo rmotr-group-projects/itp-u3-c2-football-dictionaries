5e9e21091ab69a13b060b7161cf2b626cbf9862f
Bob LePera <blepera91@gmail.com>
2018-06-27 04:19:21 +0000

    Football Dictionaries Project 1

--- football_dictionaries/assignment_2.py
+++ football_dictionaries/assignment_2.py
@@ -1,2 +1,17 @@
 def players_by_position(squads_list):
-    pass
+    positions={}
+    for i in squads_list:
+        player= {
+        "number" : i[0],
+        "position" : i[1],
+        "name" : i[2],
+        "date_of_birth" : i[3],
+        "caps" : i[4],
+        "club" : i[5],
+        "country" : i[6],
+        "club_country" : i[7],
+        "year" : i[8]
+        }
+        positions[player["position"]] = positions.get(player["position"],[])
+        positions[player["position"]].append(player)
+    return positions
