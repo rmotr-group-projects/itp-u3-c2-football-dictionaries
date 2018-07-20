7915bf2bb88b31063db3f6c28dc33e9e17ccae50
Jordan Winfery <jwinf843@gmail.com>
2018-07-20 01:10:10 +0000

    Doop doo doo doo doop

--- football_dictionaries/assignment_1.py
+++ football_dictionaries/assignment_1.py
@@ -1,2 +1,23 @@
+def transform_list_player_to_dict(player_as_list):
+    player_dictionary = {
+    'caps' : player_as_list[4],
+    'club' : player_as_dict[5],
+    'club_country' : player_as_list[7],
+    'country' : player_as_list[6],
+    'date_of_birth' : player_as_list[3],
+    'name' : player_as_list[2],
+    'number' : player_as_list[0],
+    'position' : player_as_list[1],
+    'year' : player_as_list[8],
+    }
+    
+    return player_dictionary
+
 def players_as_dictionaries(squads_list):
-    pass
+    list_of_players = []
+    for player in squads_list:
+        player_dictionary = transform_list_player_to_dict(player)
+        list_of_players.append(player_dictionary)
+    
+    return list_of_players
+    
