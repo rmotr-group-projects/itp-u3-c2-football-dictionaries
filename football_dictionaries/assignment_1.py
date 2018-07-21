26f8cdf182e91b3df0e5c62c6daa78fcfdad897b
CP Jain <cpjain2007@gmail.com>
2018-07-21 11:40:37 +0000

    Football dictionary assignment

--- football_dictionaries/assignment_1.py
+++ football_dictionaries/assignment_1.py
@@ -1,2 +1,15 @@
 def players_as_dictionaries(squads_list):
-    pass
+    result_list = []
+    for index, item in enumerate(squads_list):
+        temp_dict = {}
+        temp_dict["number"] = item[0]
+        temp_dict["position"] = item[1]
+        temp_dict["name"] = item[2]
+        temp_dict["date_of_birth"] = item[3]
+        temp_dict["caps"] = item[4]
+        temp_dict["club"] = item[5]
+        temp_dict["country"] = item[6]
+        temp_dict["club_country"] = item[7]
+        temp_dict["year"] = item[8]
+        result_list.append(temp_dict)
+    return result_list
