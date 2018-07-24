785c9a00b238461d123c85e15268974d954ebcea
Matt McClanahan <mmcclana24@yahoo.com>
2018-07-24 02:24:53 +0000

    finished project 1

--- football_dictionaries/assignment_2.py
+++ football_dictionaries/assignment_2.py
@@ -1,2 +1,14 @@
+from .assignment_1 import transform_list_player_to_dictionary
 def players_by_position(squads_list):
-    pass
+    players= {}
+        
+    
+    
+    for player_as_list in squads_list:
+        player= transform_list_player_to_dictionary(player_as_list)
+        position= player["position"]
+        players.setdefault(position,[])
+        players[position].append(player)
+    return players
+
+
--- football_dictionaries/assignment_3.py
+++ football_dictionaries/assignment_3.py
@@ -1,2 +1,25 @@
+from .assignment_1 import transform_list_player_to_dictionary
 def players_by_country_and_position(squads_list):
-    pass
+    players={}
+    
+    for player_as_list in squads_list:
+        player=transform_list_player_to_dictionary(player_as_list)
+        country=player['country']
+        players.setdefault(country,{})
+        position=player['position']
+        players[country].setdefault(position,[])
+        players[country][position].append(player)
+    return players
+        
+    
+    
+        
+        
+        
+        
+
+
+        
+        
+
+        
