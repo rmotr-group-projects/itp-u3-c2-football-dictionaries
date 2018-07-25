abad4413da174ca71828f6080764d13cbe4e0de6
RadleyESidwellLewis <rad.lewis@zoho.com>
2018-07-25 00:33:15 +0000

    Radley types his commit message here

--- football_dictionaries/assignment_3.py
+++ football_dictionaries/assignment_3.py
@@ -1,20 +1,20 @@
 def players_by_country_and_position(squads_list): 
-    country_dict = {} 
-        for index, item in enumerate(squads_list):
-        adder = {} 
-        adder["number"] = item[0] 
-        adder["position"] = item[1] 
-        adder["name"] = item[2]
-        adder["date_of_birth"] = item[3] 
-        adder["caps"] = item[4] 
-        adder["club"] = item[5] 
-        adder["country"] = item[6] 
-        adder["club_country"] = item[7]
-        adder["year"] = item[8] 
-        country_dict.setdefault(adder["country"], {}) 
-        country_dict[adder["country"]].setdefault(adder["position"], [])
-        country_dict[adder["country"]][adder["position"]].append(adder) 
-    return country_dict 
+  country_dict = {} 
+    for index, item in enumerate(squads_list):
+    adder = {} 
+    adder["number"] = item[0] 
+    adder["position"] = item[1] 
+    adder["name"] = item[2]
+    adder["date_of_birth"] = item[3] 
+    adder["caps"] = item[4] 
+    adder["club"] = item[5] 
+    adder["country"] = item[6] 
+    adder["club_country"] = item[7]
+    adder["year"] = item[8] 
+    country_dict.setdefault(adder["country"], {}) 
+    country_dict[adder["country"]].setdefault(adder["position"], [])
+    country_dict[adder["country"]][adder["position"]].append(adder) 
+  return country_dict 
 
 
 
