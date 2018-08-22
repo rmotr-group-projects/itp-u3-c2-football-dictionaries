170125a4d271b258d1f27d1de299129e6d09a1ce
Todd <appalachee.red@gmail.com>
2018-08-21 20:41:48 -0400

    passed test

--- football_dictionaries/assignment_2.py
+++ football_dictionaries/assignment_2.py
@@ -1,2 +1,23 @@
+from pprint import pprint
+
 def players_by_position(squads_list):
-    pass
+
+    by_position = {}
+
+    for player in squads_list:
+        player_dict = {
+            'number': player[0],
+            'position': player[1],
+            'name': player[2],
+            'date_of_birth': player[3],
+            'caps': player[4],
+            'club': player[5],
+            'country': player[6],
+            'club_country': player[7],
+            'year': player[8],
+        }
+
+        position = player_dict['position']
+        by_position.setdefault(position, [])
+        by_position[position].append(player_dict)
+    return by_position
