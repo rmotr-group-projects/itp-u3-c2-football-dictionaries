562b871ccb4cd4216a850ac75fcf43ddca4f5e8d
Todd <appalachee.red@gmail.com>
2018-08-23 11:29:21 -0400

    tests passed

--- warm_up/assignment_4.py
+++ warm_up/assignment_4.py
@@ -1,2 +1,7 @@
 def group_users_by_email_domain(users):
-    pass
+    results = {}
+    for user in users:
+        domain = user['email'].split('@')[1]
+        results.setdefault(domain, [])
+        results[domain].append(user)
+    return results
