78c953cba2cb9aa4153ae532f8dba02535fa5d50
Jae <jaejunkwak@outlook.com>
2018-08-23 02:49:57 +0000

    Finished the project

--- football_dictionaries/assignment_2.py
+++ football_dictionaries/assignment_2.py
@@ -1,2 +1,11 @@
-def players_by_position(squads_list):
-    pass
+from squads_data import SQUADS_DATA
+from assignment_1 import players_as_dictionaries
+
+def players_by_position(SQUADS_DATA):
+    squads_list=players_as_dictionaries(SQUADS_DATA)
+    new_dict={}
+    for player in squads_list:
+        new_dict.setdefault(player['position'],[])
+        new_dict[player['position']].append(player)
+    return new_dict
+    
--- football_dictionaries/assignment_3.py
+++ football_dictionaries/assignment_3.py
@@ -1,2 +1,18 @@
-def players_by_country_and_position(squads_list):
-    pass
+from squads_data import SQUADS_DATA
+from assignment_2 import players_by_position
+import pprint
+
+def players_by_country_and_position(SQUADS_DATA):
+    squads_list=players_by_position(SQUADS_DATA)
+    new_country_dict={}
+    
+    for key,pos_dict in squads_list.items():
+        
+        for pos_player in pos_dict:
+            new_country_dict.setdefault(pos_player['country'],{})
+            new_country_dict[pos_player['country']].setdefault(pos_player['position'],[])
+            new_country_dict[pos_player['country']][pos_player['position']].append(pos_player)
+    
+    return new_country_dict
+
+#pprint.pprint(players_by_country_and_position(SQUADS_DATA))
