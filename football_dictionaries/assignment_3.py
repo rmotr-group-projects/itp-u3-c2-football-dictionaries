9d9719dafc92f6adef783a8aeea02bb53cc6c60c
Joey Cao <caoyy94@hotmail.com>
2018-08-26 02:05:55 +0000

    passing tests

--- football_dictionaries/assignment_3.py
+++ football_dictionaries/assignment_3.py
@@ -1,2 +1,16 @@
+from football_dictionaries.assignment_1 import players_as_dictionaries
+
 def players_by_country_and_position(squads_list):
+    new_list = players_as_dictionaries(squads_list)
+    country_dict = {}
+ 
+    for player in new_list:
+        country = player['country']
+        pos = player['position']
+    
+        country_dict.setdefault(country,{})
+        country_dict[country].setdefault(pos,[])
+        country_dict[country][pos].append(player)
+     
+    return country_dict
     pass
