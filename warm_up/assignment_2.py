2a971e49d8f0a62e1bf4ad026095c1b53249647a
Donald Cesaretti <daibando413@gmail.com>
2018-08-27 17:09:06 +0000

    Fixed all errors found. Final submission.

--- warm_up/assignment_2.py
+++ warm_up/assignment_2.py
@@ -1,2 +1,12 @@
+# PASSED warm_up assignment_2
+
 def transform_user_list_to_dict(a_user):
-    pass
+    users = {
+        'name': a_user[0],
+        'email': a_user[1],
+        'age': int(a_user[2]),
+    }
+    
+    return users
+     
+#transform_user_list_to_dict(['Don', 'don@don.com', 27])
