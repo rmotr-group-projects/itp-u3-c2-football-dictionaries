e6f7e119686fdf30d8a8bcec694c6157fe71b822
EC2 Default User <ec2-user@ip-172-31-59-102.ec2.internal>
2018-09-20 16:01:20 +0000

    Nested list assignments

--- football_dictionaries/assignment_3.py
+++ football_dictionaries/assignment_3.py
@@ -1,2 +1,14 @@
 def players_by_country_and_position(squads_list):
-    pass
+    country = {}
+    holder = {}
+    for player in squads_list:
+        pos = player[1]
+        cntry = player[6]
+        if not cntry in country.keys():
+            holder[cntry] = {}
+            country[cntry] = []
+        if not pos in country[cntry]:
+            holder[cntry][pos] = []
+            country[cntry].append(pos)
+        holder[cntry][pos].append({'number':player[0],'position':player[1],'name':player[2],'date_of_birth':player[3],'caps':player[4],'club':player[5],'country':player[6],'club_country':player[7],'year':player[8]})
+    return holder
--- warm_up/assignment_4.py
+++ warm_up/assignment_4.py
@@ -1,2 +1,11 @@
 def group_users_by_email_domain(users):
-    pass
+    emails = []
+    holder = {}
+    for user in users:
+        domain = user['email'].split('@')[1].rstrip()
+        if not domain in emails:
+            holder[domain] = []
+            emails.append(domain)
+        holder[domain].append(user)
+    return holder
+
