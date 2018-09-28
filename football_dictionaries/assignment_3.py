244cb8da6d750cd1aa9e83ade73542e34fa96e0e
Raghu Reddy <raghupama@gmail.com>
2018-09-28 03:49:17 +0000

    Finish football dictionaries assignment

--- football_dictionaries/assignment_3.py
+++ football_dictionaries/assignment_3.py
@@ -1,2 +1,18 @@
+from .assignment_1 import players_as_dictionaries
+
 def players_by_country_and_position(squads_list):
-    pass
+    player_dict = players_as_dictionaries(squads_list)
+    countries = {}
+    
+    for player in player_dict:
+        countries.setdefault(player['country'], {})
+        countries[player['country']].setdefault(player['position'], [])
+        
+        print('-------')
+        print(countries)
+        
+        thisCountry = countries[player["country"]]
+        thisCountry[player['position']].append(player)
+        
+        # countries[player['country']['position']].append(player)
+    return countries
