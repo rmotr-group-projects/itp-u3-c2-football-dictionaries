4717db7f8e09ae7a7d681bc356998f5fdf916a24
Raghu Reddy <raghupama@gmail.com>
2018-09-28 03:06:49 +0000

    Finish warmup exercises

--- warm_up/assignment_4.py
+++ warm_up/assignment_4.py
@@ -1,2 +1,11 @@
 def group_users_by_email_domain(users):
-    pass
+    all_domains = {}
+    for user in users:
+        current_domain = parse_email_domains_from_user_email(user)
+        all_domains.setdefault(current_domain, [])
+        all_domains[current_domain].append(user)
+    return all_domains
+
+
+def parse_email_domains_from_user_email(user):
+    return user['email'].split("@")[1]
