f6600695320eabaf6f8771ebb45211787d088e5f
Dave P <david.przywara@gmail.com>
2018-09-29 16:55:33 +0000

    Homework Complete!

--- football_dictionaries/assignment_3.py
+++ football_dictionaries/assignment_3.py
@@ -1,2 +1,15 @@
+from football_dictionaries.assignment_1 import players_as_dictionaries
+
 def players_by_country_and_position(squads_list):
-    pass
+    
+    countries = {}
+    
+    players = players_as_dictionaries(squads_list)
+    for player in players:
+        position = player['position']
+        country = player['country']
+        countries.setdefault(country, {})
+        countries[country].setdefault(position, [])
+        countries[country][position].append(player)
+    
+    return countries
--- warm_up/assignment_3.py
+++ warm_up/assignment_3.py
@@ -1,2 +1,14 @@
 def transform_multiple_users_to_dicts(multiple_users):
-    pass
+    user_list = []
+    
+    for user in multiple_users:
+        user_d = {
+            'name': user[0],
+            'email': user[1],
+            'age': user[2],
+        }
+        
+        user_list.append(user_d)
+        
+    return user_list
+        
