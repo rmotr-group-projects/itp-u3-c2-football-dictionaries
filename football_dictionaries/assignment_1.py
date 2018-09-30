4f2ff44616c0a8b0f58204a116da499141052d3b
Nick Van Kuren <nicholasvk@gmail.com>
2018-09-30 15:00:40 +0000

    Passing all  tests for warm_up and football assignment

--- football_dictionaries/assignment_1.py
+++ football_dictionaries/assignment_1.py
@@ -1,2 +1,27 @@
-def players_as_dictionaries(squads_list):
-    pass
+import csv
+from csv_example import read_squad_file
+from pprint import pprint
+# Data
+
+squad = read_squad_file(file_name='squads.csv')
+
+
+def players_as_dictionaries(squad):
+    sq_list = []
+    
+    for player in squad:
+        player_dict = {
+            'caps': player[-5],
+            'club': player[-4],
+            'club_country' : player[-2],
+            'country' : player[-3],
+            'date_of_birth' : player[3],
+            'name': player[2],
+            'number' : player[0],
+            'position' : player[1],
+            'year' : player[-1],
+        }
+    
+        sq_list.append(player_dict)
+    
+    return sq_list
--- warm_up/assignment_4.py
+++ warm_up/assignment_4.py
@@ -1,2 +1,12 @@
+      
 def group_users_by_email_domain(users):
-    pass
+    user_dict = {}
+
+    for user in users:
+        domain = user['email'].split('@')[1]
+        user_dict.setdefault(domain, [])
+        user_dict[domain].append(user)
+    
+    return user_dict
+            
+        
