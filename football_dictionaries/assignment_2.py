459c9ae11a19d7055da3f1a2f333975c995d1e9a
Yousaf <yousafmufc@gmail.com>
2018-10-01 23:15:54 +0000

    Cleaned up assignment 2 and 3 of football dictionaries

--- football_dictionaries/assignment_2.py
+++ football_dictionaries/assignment_2.py
@@ -1,6 +1,24 @@
-from football_dictionaries.squads_data import SQUADS_DATA
-from football_dictionaries.assignment_1 import players_as_dictionaries
-
+def players_as_dictionaries(squads_list):
+    
+    players_as_dict = []
+    
+    for player in squads_list:
+        
+        player_dict = {
+            'number': player[0],
+            'position': player[1],
+            'name': player[2],
+            'date_of_birth': player[3],
+            'caps': player[4],
+            'club': player[5],
+            'country': player[6],
+            'club_country': player[7],
+            'year': player[8],
+            
+        }
+        
+        players_as_dict.append(player_dict)
+    return players_as_dict
 #-------------------------------------------------------------------------------
 def players_by_position(squads_list):
     
@@ -23,164 +41,3 @@ def players_by_position(squads_list):
     
     return dict_of_positions
 
-SQUADS_DATA = [
-  [
-    "1",
-    "GK",
-    "Juan Botasso",
-    "(1908-10-23)23 October 1908 (aged 21)",
-    "",
-    "Quilmes",
-    "Argentina",
-    "Argentina",
-    "1930"
-  ],
-  [
-    "9",
-    "FW",
-    "Roberto Cherro",
-    "(1907-02-23)23 February 1907 (aged 23)",
-    "",
-    "Boca Juniors",
-    "Argentina",
-    "Argentina",
-    "1930"
-  ],
-  [
-    "-",
-    "MF",
-    "Pierre Braine",
-    "(1900-10-26)26 October 1900 (aged 29)",
-    "42",
-    "Royal Beerschot AC",
-    "Belgium",
-    "Belgium",
-    "1930"
-  ],
-  [
-    "-",
-    "MF",
-    "Alexis Chantraine",
-    "(1901-03-16)16 March 1901 (aged 29)",
-    "0",
-    "Royal FC Liegeois",
-    "Belgium",
-    "Belgium",
-    "1930"
-  ],
-  [
-    "-",
-    "GK",
-    "Jean De Bie",
-    "(1892-05-09)9 May 1892 (aged 38)",
-    "37",
-    "Royal Racing Club de Bruxelles",
-    "Belgium",
-    "Belgium",
-    "1930"
-  ],
-  [
-    "-",
-    "MF",
-    "Oscar",
-    "(1991-09-09)9 September 1991 (aged 22)",
-    "29",
-    "Chelsea",
-    "Brazil",
-    "England",
-    "2010"
-  ],
-  [
-    "-",
-    "MF",
-    "Paulinho",
-    "(1988-07-25)25 July 1988 (aged 25)",
-    "25",
-    "Tottenham Hotspur",
-    "Brazil",
-    "England",
-    "2010"
-  ],
-  [
-    "-",
-    "MF",
-    "Hernanes",
-    "(1985-05-29)29 May 1985 (aged 29)",
-    "23",
-    "Internazionale",
-    "Brazil",
-    "Italy",
-    "2014"
-  ],
-  [
-    "-",
-    "MF",
-    "Luiz Gustavo",
-    "(1987-07-23)23 July 1987 (aged 26)",
-    "17",
-    "VfL Wolfsburg",
-    "Brazil",
-    "Germany",
-    "2014"
-  ],
-  [
-    "-",
-    "MF",
-    "Fernandinho",
-    "(1985-05-04)4 May 1985 (aged 29)",
-    "6",
-    "Manchester City",
-    "Brazil",
-    "England",
-    "2014"
-  ],
-  [
-    "-",
-    "MF",
-    "Willian",
-    "(1988-08-09)9 August 1988 (aged 25)",
-    "5",
-    "Chelsea",
-    "Brazil",
-    "England",
-    "2014"
-  ],
-  [
-    "-",
-    "FW",
-    "Lee Keun-Ho",
-    "(1985-04-11)11 April 1985 (aged 29)",
-    "62",
-    "Sangju Sangmu",
-    "South Korea",
-    "South Korea",
-    "2014"
-  ],
-  [
-    "-",
-    "FW",
-    "Koo Ja-Cheol",
-    "(1989-02-27)27 February 1989 (aged 25)",
-    "35",
-    "Mainz 05",
-    "South Korea",
-    "Germany",
-    "2014"
-  ],
-  [
-    "-",
-    "FW",
-    "Kim Shin-Wook",
-    "(1988-04-14)14 April 1988 (aged 26)",
-    "26",
-    "Ulsan Hyundai",
-    "South Korea",
-    "South Korea",
-    "2014"
-  ]
-]
-
-
-print(players_by_position(SQUADS_DATA))
-        
-    
