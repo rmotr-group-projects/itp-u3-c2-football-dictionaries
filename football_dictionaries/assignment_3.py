6939cca75c7d78bfe8fc198599e76038242c3832
mikoumed <mikoumed87@gmail.com>
2018-10-02 20:04:51 +0000

    get all tests passing

--- football_dictionaries/assignment_3.py
+++ football_dictionaries/assignment_3.py
@@ -1,2 +1,25 @@
 def players_by_country_and_position(squads_list):
-    pass
+    squads_data = []
+    
+    for player in squads_list:
+        dictionary = {}
+        dictionary['number']        = player[0]
+        dictionary['position']      = player[1]
+        dictionary['name']          = player[2]
+        dictionary['date_of_birth'] = player[3]
+        dictionary['caps']          = player[4]
+        dictionary['club']          = player[5]
+        dictionary['country']       = player[6]
+        dictionary['club_country']  = player[7]
+        dictionary['year']          = player[8]
+        
+        squads_data.append(dictionary)
+        
+    dictionary = {}
+    for player in squads_data:
+
+        dictionary.setdefault(player['country'], {})
+        dictionary[player['country']].setdefault(player['position'],[])
+        dictionary[player['country']][player['position']].append(player)
+
+    return dictionary
