7577952770b9139b97362f8c65dc7917c4675a88
adilnaimi <adilnaimi@gmail.com>
2018-10-12 12:35:10 +0300

    add  solution

--- football_dictionaries/assignment_3.py
+++ football_dictionaries/assignment_3.py
@@ -1,2 +1,15 @@
+from football_dictionaries.assignment_1 import players_as_dictionaries
+
 def players_by_country_and_position(squads_list):
-    pass
+    countries_list = {}
+
+    players = players_as_dictionaries(squads_list)
+
+    for player in players:
+        position = player['position']
+        country = player['country']
+        countries_list.setdefault(country, {})
+        countries_list[country].setdefault(position, [])
+        countries_list[country][position].append(player)
+
+    return countries_list
