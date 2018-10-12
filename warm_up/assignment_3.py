30b6172abe7cb42a31e6028d695abc6492b20c01
adilnaimi <adilnaimi@gmail.com>
2018-10-12 11:15:58 +0300

    add warmup solution

--- warm_up/assignment_3.py
+++ warm_up/assignment_3.py
@@ -1,2 +1,12 @@
 def transform_multiple_users_to_dicts(multiple_users):
-    pass
+    users_list = []
+    for a_user in multiple_users:
+        user = {
+            'name': a_user[0],
+            'email': a_user[1],
+            'age': a_user[2],
+        }
+
+        users_list.append(user)
+    return  users_list
+
