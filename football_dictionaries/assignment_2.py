01861dc6afc91a84b1f44061c0c9db900c0ff07c
Daniele Bongiovanni <danibongio@gmail.com>
2018-10-26 03:17:45 +0000

    All tests passed

--- football_dictionaries/assignment_2.py
+++ football_dictionaries/assignment_2.py
@@ -1,4 +1,22 @@
-from football_dictionaries.assignment_1 import players_as_dictionaries
+#from football_dictionaries.assignment_1 import players_as_dictionaries
+
+def players_as_dictionaries(squads_list):
+    football_list=[]
+    for player in squads_list:
+        football_player={
+            'number': player[0],
+            'position': player[1],
+            'name': player[2],
+            'date_of_birth': player[3],
+            'caps': player[4],
+            'club': player[5],
+            'country': player[6],
+            'club_country': player[7],
+            'year': player[8],
+            }
+        football_list.append(football_player)
+    return football_list
+
 
 def players_by_position(squads_list):
     result={}
