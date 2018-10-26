e9e03c2b889480edac53814d9023863f00ef7d00
Daniele Bongiovanni <danibongio@gmail.com>
2018-10-26 02:44:15 +0000

    Completed Week 3 Project - All tests passed!

--- football_dictionaries/assignment_3.py
+++ football_dictionaries/assignment_3.py
@@ -1,2 +1,14 @@
+from football_dictionaries.assignment_1 import players_as_dictionaries
+
 def players_by_country_and_position(squads_list):
-    pass
+    result={}
+    football_list=players_as_dictionaries(squads_list)
+    
+    for player in football_list:
+        country=player['country']
+        result.setdefault(country,{})
+        position=player['position']
+        result[country].setdefault(position,[])
+        result[country][position].append(player)
+        
+    return result
