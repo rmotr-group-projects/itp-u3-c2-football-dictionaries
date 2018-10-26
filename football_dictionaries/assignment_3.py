64023ecf992bcb9791a32f36e9a49226334333b7
Daniele Bongiovanni <danibongio@gmail.com>
2018-10-26 03:42:26 +0000

    Cleaned up files

--- football_dictionaries/assignment_3.py
+++ football_dictionaries/assignment_3.py
@@ -1,22 +1,4 @@
-#from football_dictionaries.assignment_1 import players_as_dictionaries
-
-def players_as_dictionaries(squads_list):
-    football_list=[]
-    for player in squads_list:
-        football_player={
-            'number': player[0],
-            'position': player[1],
-            'name': player[2],
-            'date_of_birth': player[3],
-            'caps': player[4],
-            'club': player[5],
-            'country': player[6],
-            'club_country': player[7],
-            'year': player[8],
-            }
-        football_list.append(football_player)
-    return football_list
-
+from football_dictionaries.assignment_1 import players_as_dictionaries
 
 def players_by_country_and_position(squads_list):
     result={}
