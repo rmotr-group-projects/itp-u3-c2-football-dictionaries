a554b38bc6d70c0ef43b34a099ec22b5b4ddd146
Michael Harper <mind.the.data@gmail.com>
2018-10-27 14:12:23 +0000

    assignment_2.py refactor

--- football_dictionaries/assignment_2.py
+++ football_dictionaries/assignment_2.py
@@ -1,19 +1,26 @@
 from football_dictionaries.assignment_1 import players_as_dictionaries
 
+def position_sort(position_dict, player, position):
+    if player['position'] == position:
+        position_dict[position].append(player)
+    return position_dict
+
 def players_by_position(squads_list):
     player_dict_list = players_as_dictionaries(squads_list)
     position_list = ['GK', 'FW', 'MF']
-    res = {}
+    position_dict = {}
     for position in position_list:
-        res.setdefault(position, [])
+        position_dict.setdefault(position, [])
     # res.setdefault('GK', [])
     # res.setdefault('MF', [])
     # res.setdefault('FW', [])
     for player in player_dict_list:
-        if player['position'] == 'GK':
-            res['GK'].append(player)
-        if player['position'] == 'FW':
-            res['FW'].append(player)
-        if player['position'] == 'MF':
-            res['MF'].append(player)
-    return res
+        for position in position_list:
+            position_dict = position_sort(position_dict, player, position)
+        # if player['position'] == 'GK':
+        #     position_dict['GK'].append(player)
+        # if player['position'] == 'FW':
+        #     position_dict['FW'].append(player)
+        # if player['position'] == 'MF':
+        #     position_dict['MF'].append(player)
+    return position_dict
