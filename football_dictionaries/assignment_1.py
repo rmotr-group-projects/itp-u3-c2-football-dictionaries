d92d1e96643808ea66a1a3a8c6190a11f23940b3
Michael Harper <mind.the.data@gmail.com>
2018-10-27 13:46:25 +0000

    Project complete after refactor

--- football_dictionaries/assignment_1.py
+++ football_dictionaries/assignment_1.py
@@ -1,2 +1,26 @@
 def players_as_dictionaries(squads_list):
-    pass
+    res = []
+    for player in squads_list:
+        build = {}
+        build.setdefault('number', player[0])
+        build.setdefault('position', player[1])
+        build.setdefault('name', player[2])
+        build.setdefault('date_of_birth', player[3])
+        build.setdefault('caps', player[4])
+        build.setdefault('club', player[5])
+        build.setdefault('country', player[6])
+        build.setdefault('club_country', player[7])
+        build.setdefault('year', player[8])
+       # build = {
+        #    'number': player[0],
+         #   'position': player[1],
+          #  'name': player[2],
+           # 'date_of_birth': player[3],
+        #    'caps': player[4],
+         #   'club': player[5],
+         #   'country': player[6],
+          #  'club_country': player[7],
+           # 'year': player[8]
+        #}
+        res.append(build)
+    return res
--- football_dictionaries/assignment_2.py
+++ football_dictionaries/assignment_2.py
@@ -1,2 +1,16 @@
+from football_dictionaries.assignment_1 import players_as_dictionaries
+
 def players_by_position(squads_list):
-    pass
+    player_dict_list = players_as_dictionaries(squads_list)
+    res = {}
+    res.setdefault('GK', [])
+    res.setdefault('MF', [])
+    res.setdefault('FW', [])
+    for player in player_dict_list:
+        if player['position'] == 'GK':
+            res['GK'].append(player)
+        if player['position'] == 'FW':
+            res['FW'].append(player)
+        if player['position'] == 'MF':
+            res['MF'].append(player)
+    return res
