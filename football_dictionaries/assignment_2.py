85787857500f556fb23359ef2408fcdb7ab92293
Anders Bill <anders@darkroom.tech>
2018-10-27 16:28:46 +0000

    weekend 1 assignment finished

--- football_dictionaries/assignment_2.py
+++ football_dictionaries/assignment_2.py
@@ -1,2 +1,12 @@
+from football_dictionaries.assignment_1 import players_as_dictionaries
+
 def players_by_position(squads_list):
-    pass
+    result = {}
+    player_dict = players_as_dictionaries(squads_list)
+    for player in player_dict:
+        #goal is to put values in result dict of positions
+        position = player['position']
+        result.setdefault(position, [])
+        #goal is to get the player information of that position into the value of the position key
+        result[position].append(player)
+    return result
