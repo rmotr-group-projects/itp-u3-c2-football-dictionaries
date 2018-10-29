bae3210ff169592863a855c54e146dbd37fa410e
Sarah <sarahsong0302@gmail.com>
2018-10-29 01:48:29 +0000

    pushing test 1

--- football_dictionaries/assignment_3.py
+++ football_dictionaries/assignment_3.py
@@ -1,2 +1,44 @@
+
+from football_dictionaries.squads_data import SQUADS_DATA
+
+def player_list_to_dict(list_of_player):
+    player_dictionaries = {
+        'caps': list_of_player[4],
+        'club': list_of_player[5],
+        'club_country': list_of_player[7],
+        'country': list_of_player[6],
+        'date_of_birth': list_of_player[3],
+        'name': list_of_player[2],
+        'number': list_of_player[0],
+        'position': list_of_player[1],
+        'year': list_of_player[8]
+    }
+    return player_dictionaries
+
+def players_by_position(squads_list):
+    players = {
+        "GK": [],
+        "MF": [],
+        "FW": []
+    }
+    for player_list in squads_list:
+        player = player_list_to_dict(player_list)
+        position = player['position']
+        players[position].append(player)
+    return players
+
 def players_by_country_and_position(squads_list):
-    pass
+    players = {
+        'Argentina': [], 
+        'Belgium': [], 
+        'Brazil': [], 
+        'South Korea': []
+    }
+    for player_list in squads_list:
+        player = player_by_position(squads_list)
+        country = player['country']
+        players[country].append(player)
+    
+    return players
+
+print(players_by_position(SQUADS_DATA))
