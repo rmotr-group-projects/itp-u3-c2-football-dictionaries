17a879afa4f98ba3ee354c44571725a2bff73f96
Manasa <manasa75@gmail.com>
2018-10-29 04:29:46 +0000

    Updated code

--- football_dictionaries/assignment_1.py
+++ football_dictionaries/assignment_1.py
@@ -1,2 +1,23 @@
+def players_list_as_dict(user):
+   player_dict = {
+       'caps': user[4],
+       'club': user[5],
+       'club_country': user[7],
+       'country': user[6],
+       'date_of_birth': user[3],
+       'name': user[2],
+       'number': user[0],
+       'position': user[1],
+       'year': user[8]
+    }
+   return player_dict
+
+
+
 def players_as_dictionaries(squads_list):
-    pass
+    players= []
+    for user in squads_list:
+        player_dict=players_list_as_dict(user)
+        players.append(player_dict)
+    return players
+
