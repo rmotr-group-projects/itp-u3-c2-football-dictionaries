7dc9e32afaeae4a47a8d5fdb5f0158b6f82f324b
Michael <meeker.michael@gmail.com>
2018-11-14 03:18:05 +0000

    test3

--- test_warmup.py
+++ test_warmup.py
@@ -1,141 +1,135 @@
-# Test 1
-
-assert build_user_as_dict('John', 'john@gmail.com', 31) == {
-    'name': 'John',
-    'email': 'john@gmail.com',
-    'age': 31
-}
+from warm_up.assignment_1 import build_user_as_dict
+from warm_up.assignment_2 import transform_user_list_to_dict
+from warm_up.assignment_3 import transform_multiple_users_to_dicts
+from warm_up.assignment_4 import group_users_by_email_domain
 
 
+def test_assignment_1():
+    user_1 = build_user_as_dict('John', 'john@gmail.com', 31)
+    assert user_1 == {
+        'name': 'John',
+        'email': 'john@gmail.com',
+        'age': 31
+    }
 
-assert build_user_as_dict('Mary', 'mary@hotmail.com', 28) == {
-    'name': 'Mary',
-    'email': 'mary@hotmail.com',
-    'age': 28
-}
-
-
-# Test 2
-
-
-
-l1 = ['John', 'john@gmail.com', 31]
-
-assert transform_user_list_to_dict(l1) == {
-    'name': 'John',
-    'email': 'john@gmail.com',
-    'age': 31
-}
-
-l2 = ['Mary', 'mary@hotmail.com', 28]
-
-assert transform_user_list_to_dict(l2) == {
-    'name': 'Mary',
-    'email': 'mary@hotmail.com',
-    'age': 28
-}
+    user_1 = build_user_as_dict('Mary', 'mary@hotmail.com', 28)
+    assert user_1 == {
+        'name': 'Mary',
+        'email': 'mary@hotmail.com',
+        'age': 28
+    }
 
 
-# Test 3
+def test_assignment_2():
+    user_list_1 = ['John', 'john@gmail.com', 31]
+    user_dict_1 = transform_user_list_to_dict(user_list_1)
+
+    assert user_dict_1 == {
+        'name': 'John',
+        'email': 'john@gmail.com',
+        'age': 31
+    }
+
+    user_list_2 = ['Mary', 'mary@hotmail.com', 28]
+    user_dict_2 = transform_user_list_to_dict(user_list_2)
+
+    assert user_dict_2 == {
+        'name': 'Mary',
+        'email': 'mary@hotmail.com',
+        'age': 28
+    }
 
 
-users = [
-    ['John', 'john@gmail.com', 31],
-    ['Mary', 'mary@hotmail.com', 28],
-    ['Rose', 'rose@yahoo.com', 30]
-]
+def test_assignment_3():
+    user_list_1 = ['John', 'john@gmail.com', 31]
+    user_list_2 = ['Mary', 'mary@hotmail.com', 28]
+    user_list_3 = ['Rose', 'rose@yahoo.com', 30]
 
-users_as_dict = transform_multiple_users_to_dicts(users)
+    users = [
+        user_list_1,
+        user_list_2,
+        user_list_3,
+    ]  # IMPORTANT! First nested collection. This is a "list of lists"
 
-assert users_as_dict == [{
-    'name': 'John',
-    'email': 'john@gmail.com',
-    'age': 31
-}, {
-    'name': 'Mary',
-    'email': 'mary@hotmail.com',
-    'age': 28
-}, {
-    'name': 'Rose',
-    'email': 'rose@yahoo.com',
-    'age': 30
-}]
+    users_as_dict = transform_multiple_users_to_dicts(users)
+    assert users_as_dict == [{
+        'name': 'John',
+        'email': 'john@gmail.com',
+        'age': 31
+    }, {
+        'name': 'Mary',
+        'email': 'mary@hotmail.com',
+        'age': 28
+    }, {
+        'name': 'Rose',
+        'email': 'rose@yahoo.com',
+        'age': 30
+    }]
 
-
-
-# Test 4
-
-
-
-
-
-users = [{
-    'name': 'John',
-    'email': 'john@gmail.com',     # gmail
-    'age': 31
-}, {
-    'name': 'Mary',
-    'email': 'mary@hotmail.com',   # hotmail
-    'age': 28
-}, {
-    'name': 'Rose',
-    'email': 'rose@yahoo.com',     # yahoo
-    'age': 30
-}, {
-    'name': 'Jane',
-    'email': 'jane@gmail.com',     # gmail
-    'age': 25
-}, {
-    'name': 'Dustin',
-    'email': 'dustin@hotmail.com',  # hotmail
-    'age': 35
-}]
-
-users_grouped = group_users_by_email_domain(users)
-
-assert users_grouped == {
-    'gmail.com': [{
+def test_assignment_4():
+    users = [{
         'name': 'John',
         'email': 'john@gmail.com',     # gmail
         'age': 31
     }, {
-        'name': 'Jane',
-        'email': 'jane@gmail.com',     # gmail
-        'age': 25
-    }],
-    'hotmail.com': [{
         'name': 'Mary',
         'email': 'mary@hotmail.com',   # hotmail
         'age': 28
     }, {
-        'name': 'Dustin',
-        'email': 'dustin@hotmail.com',  # hotmail
-        'age': 35
-    }],
-    'yahoo.com': [{
         'name': 'Rose',
         'email': 'rose@yahoo.com',     # yahoo
         'age': 30
+    }, {
+        'name': 'Jane',
+        'email': 'jane@gmail.com',     # gmail
+        'age': 25
+    }, {
+        'name': 'Dustin',
+        'email': 'dustin@hotmail.com',  # hotmail
+        'age': 35
     }]
-}
 
-In [ ]:
+    users_grouped = group_users_by_email_domain(users)
+    assert users_grouped == {
+        'gmail.com': [{
+            'name': 'John',
+            'email': 'john@gmail.com',     # gmail
+            'age': 31
+        }, {
+            'name': 'Jane',
+            'email': 'jane@gmail.com',     # gmail
+            'age': 25
+        }],
+        'hotmail.com': [{
+            'name': 'Mary',
+            'email': 'mary@hotmail.com',   # hotmail
+            'age': 28
+        }, {
+            'name': 'Dustin',
+            'email': 'dustin@hotmail.com',  # hotmail
+            'age': 35
+        }],
+        'yahoo.com': [{
+            'name': 'Rose',
+            'email': 'rose@yahoo.com',     # yahoo
+            'age': 30
+        }]
+    }
 
-users = [{
-    'name': 'Jason',
-    'email': 'jason@rmotr.com',
-    'age': 61
-}]
-users_grouped = group_users_by_email_domain(users)
-
-assert users_grouped == {
-    'rmotr.com': [{
+    # Another test just in case
+    users = [{
         'name': 'Jason',
         'email': 'jason@rmotr.com',
         'age': 61
     }]
-}
+    users_grouped = group_users_by_email_domain(users)
 
-
+    assert users_grouped == {
+        'rmotr.com': [{
+            'name': 'Jason',
+            'email': 'jason@rmotr.com',
+            'age': 61
+        }]
+    }
 
 
---
+++ warm_up/assignment_1.py
@@ -0,0 +1,6 @@
+def build_user_as_dict(name, email, age):
+    return {
+        'name': name,
+        'email': email,
+        'age': age
+    }
