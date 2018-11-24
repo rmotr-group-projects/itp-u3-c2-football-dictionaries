27dc4b2ca3100b4bc95d7fda3102b584c4a6d399
Bulldg87 <frank.merla1@gmail.com>
2018-11-24 18:48:21 +0000

    football dictionary project FM upload v_1.0

--- football_dictionaries/assignment_3.py
+++ football_dictionaries/assignment_3.py
@@ -1,2 +1,13 @@
+from football_dictionaries.assignment_1 import players_as_dictionaries
+
 def players_by_country_and_position(squads_list):
-    pass
+    expected_countries = {}
+    squads_list = players_as_dictionaries(squads_list)
+    for player in squads_list:
+        country = player['country']
+        position = player['position']     
+        expected_countries.setdefault(country, {})
+        expected_countries[country].setdefault(position,[])
+        expected_countries[country][position].append(player)  
+    return expected_countries 
+
