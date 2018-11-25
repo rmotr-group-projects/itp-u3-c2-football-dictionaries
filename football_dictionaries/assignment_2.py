edfa4595a74a26f1077f73097a2b885a3b5aac3f
julysses <julyssesd@gmail.com>
2018-11-25 22:08:14 +0000

    completed footbool project - all tests passed

--- football_dictionaries/assignment_2.py
+++ football_dictionaries/assignment_2.py
@@ -1,2 +1,10 @@
+from football_dictionaries.assignment_1 import players_as_dictionaries
+
 def players_by_position(squads_list):
-    pass
+    players = players_as_dictionaries(squads_list)
+    position_dict = {}
+    for player in players:
+        position = player['position']
+        position_dict.setdefault(position,[])
+        position_dict[position].append(player)
+    return position_dict
--- football_dictionaries/assignment_3.py
+++ football_dictionaries/assignment_3.py
@@ -1,2 +1,12 @@
+from football_dictionaries.assignment_1 import players_as_dictionaries
+
 def players_by_country_and_position(squads_list):
-    pass
+    players = players_as_dictionaries(squads_list)
+    country_dict = {}
+    for player in players:
+        country = player['country']
+        position = player['position']
+        country_dict.setdefault(country, {})
+        country_dict[country].setdefault(position, [])
+        country_dict[country][position].append(player)
+    return country_dict
