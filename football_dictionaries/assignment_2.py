eda226de27d4477165987d2a40e76b357485a57b
julienbeaulieu <beaulieu.jb@gmail.com>
2018-11-25 23:45:25 +0000

    1st commit to all the 3 finished assignments

--- football_dictionaries/assignment_2.py
+++ football_dictionaries/assignment_2.py
@@ -1,2 +1,31 @@
+def players_as_dictionaries(squads_list):
+    player_list = []
+    dict_player = {}
+    for player in squads_list:
+        dict_player = {
+        'number': player[0],
+        'position': player[1],
+        'name': player[2],
+        'date_of_birth': player[3],
+        'caps': player[4],
+        'club': player[5],
+        'country': player[6],
+        'club_country': player[7],
+        'year': player[8]
+        }
+        player_list.append(dict_player)
+    return player_list
+
+
 def players_by_position(squads_list):
-    pass
+        
+    players_as_dict = players_as_dictionaries(squads_list)
+    players_by_position = {}
+    
+    for player in players_as_dict:
+        player_position = player['position']
+        players_by_position.setdefault(player_position, [])
+        players_by_position[player_position].append(player)
+    
+    return players_by_position
+
