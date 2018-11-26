7949553ad1f6a7fbe9efb290fbe47c054275e89a
Akataya <infotechpro@yahoo.com>
2018-11-26 00:12:16 +0000

    All tests have passed for all assignments.

--- football_dictionaries/assignment_1.py
+++ football_dictionaries/assignment_1.py
@@ -1,2 +1,15 @@
 def players_as_dictionaries(squads_list):
-    pass
+    new_list = []
+    for player in squads_list:
+        new_list.append({
+            'number': player[0],
+            'position': player[1],
+            'name': player[2],
+            'date_of_birth': player[3],
+            'caps': player[4],
+            'club': player[5],
+            'country': player[6],
+            'club_country': player[7],
+            'year': player[8],
+        })
+    return new_list
--- football_dictionaries/assignment_2.py
+++ football_dictionaries/assignment_2.py
@@ -1,2 +1,22 @@
 def players_by_position(squads_list):
-    pass
+    a_list = []
+    for player in squads_list:
+        a_list.append({
+            'number': player[0],
+            'position': player[1],
+            'name': player[2],
+            'date_of_birth': player[3],
+            'caps': player[4],
+            'club': player[5],
+            'country': player[6],
+            'club_country': player[7],
+            'year': player[8],
+        })
+    
+    a_dict = {}
+    for person in a_list:
+        position = person['position']
+        a_dict.setdefault(position, [])
+        a_dict[position].append(person)
+    
+    return a_dict
