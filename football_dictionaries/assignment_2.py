3bad36c6b40935e03c28f2db5a14a702d6ccdf93
jlamrin <nirmal.jayaraman@gmail.com>
2018-11-28 02:27:50 +0000

    <Tests working>

--- football_dictionaries/assignment_2.py
+++ football_dictionaries/assignment_2.py
@@ -1,2 +1,11 @@
+from football_dictionaries.assignment_1 import players_as_dictionaries
+
 def players_by_position(squads_list):
-    pass
+    squads_dict = players_as_dictionaries(squads_list)
+    player_by_post = {}
+    for squad in squads_dict:
+        position = squad['position']
+        player_by_post.setdefault(position, [])
+        player_by_post[position].append(squad)
+    return player_by_post
+
--- football_dictionaries/assignment_3.py
+++ football_dictionaries/assignment_3.py
@@ -1,2 +1,15 @@
+from football_dictionaries.assignment_1 import players_as_dictionaries
+
 def players_by_country_and_position(squads_list):
-    pass
+    squads_dict = players_as_dictionaries(squads_list)
+    result_dict = {}
+ 
+    for squad in squads_dict:
+        player_country = squad['country']
+        player_position = squad['position']
+        result_dict.setdefault(player_country, {})
+        result_dict[player_country].setdefault(player_position, [])
+        result_dict[player_country][player_position].append(squad)
+    return result_dict
+
+
