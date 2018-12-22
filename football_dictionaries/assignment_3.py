b75a4e131ec17f46a0707d08440794da01b27da9
skimtephen <skimtephen@gmail.com>
2018-12-22 23:55:24 +0000

    football project passed all tests

--- football_dictionaries/assignment_3.py
+++ football_dictionaries/assignment_3.py
@@ -1,2 +1,34 @@
+from football_dictionaries.assignment_1 import players_as_dictionaries
+
 def players_by_country_and_position(squads_list):
-    pass
+    player_list = players_as_dictionaries(squads_list)
+    players_country_position = {}
+    
+    country_set = set()
+    for player in player_list:
+        country = player['country']
+        country_set.add(country)
+    
+    player_country = dict.fromkeys(country_set, {})
+    print(player_country)
+        
+        
+    for country in player_country:
+        player_position = {}
+        for player in player_list:
+            position = player['position']
+            if player['country'] == country:
+                player_position.setdefault(position, [])
+                player_position[position].append(player)
+        player_country[country] = player_position
+    
+    print(player_country)
+    return player_country
+
+        
+    
+            
+            
+
+            
+            
