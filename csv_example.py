d6f93e7206b4644cdc985fe114df6b7f90d6df7d
troynhh <troynotspam@gmail.com>
2018-12-22 22:44:46 +0000

    Restored csv_example.py

--- csv_example.py
+++ csv_example.py
@@ -4,13 +4,8 @@ import csv
 def read_squad_file(file_name='squads.csv'):
     squad = []
     with open(file_name) as fp:
-        print('yep')
         reader = csv.reader(fp)
         for line in reader:
             squad.append(line)
 
     return squad
-
-print(read_squad_file("test_data.csv"))
-
-print("where is my code?")
