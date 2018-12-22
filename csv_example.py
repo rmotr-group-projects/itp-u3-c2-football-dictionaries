8a58aa080f6ac6401f09e0613ef4790a85717b11
troynhh <troynotspam@gmail.com>
2018-12-22 22:35:21 +0000

    Passed all tests, committing.

--- csv_example.py
+++ csv_example.py
@@ -4,8 +4,13 @@ import csv
 def read_squad_file(file_name='squads.csv'):
     squad = []
     with open(file_name) as fp:
+        print('yep')
         reader = csv.reader(fp)
         for line in reader:
             squad.append(line)
 
     return squad
+
+print(read_squad_file("test_data.csv"))
+
+print("where is my code?")
--- football_dictionaries/assignment_1.py
+++ football_dictionaries/assignment_1.py
@@ -1,2 +1,19 @@
+#from squads_data import SQUADS_DATA
+
 def players_as_dictionaries(squads_list):
-    pass
+    #list converted to dictionaries
+    list_categories = ['number','position','name','date_of_birth','caps','club','country','club_country','year']
+    
+    list_of_squad_dictionaries = []
+    
+    for one_player in squads_list:
+        #zip two lists together (combine at same index)
+        single_player_dictionary = dict(zip(list_categories,one_player))
+
+        #append a list with this data
+        list_of_squad_dictionaries.append(single_player_dictionary)
+    
+    return list_of_squad_dictionaries
+
+#DEBUG code
+#print(players_as_dictionaries(SQUADS_DATA))
