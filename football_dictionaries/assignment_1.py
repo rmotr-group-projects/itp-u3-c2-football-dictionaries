143868ad4ba40d80c1cc95d2255739b882080081
JaizWaheed <jaiz.waheed@gmail.com>
2018-12-26 06:58:09 +0000

    Completed b-29 football dictionaries project

--- football_dictionaries/assignment_1.py
+++ football_dictionaries/assignment_1.py
@@ -1,2 +1,16 @@
 def players_as_dictionaries(squads_list):
-    pass
+    result = []
+    for each in squads_list:
+        player = {
+            'number': each[0],
+            'position': each[1],
+            'name': each[2],
+            'date_of_birth': each[3],
+            'caps': each[4],
+            'club': each[5],
+            'country': each[6],
+            'club_country': each[7],
+            'year': each[8],
+        }
+        result.append(player)
+    return result
--- football_dictionaries/assignment_3.py
+++ football_dictionaries/assignment_3.py
@@ -1,2 +1,12 @@
+from football_dictionaries.assignment_1 import players_as_dictionaries
+
 def players_by_country_and_position(squads_list):
-    pass
+    result = {}
+    players = players_as_dictionaries(squads_list)
+    for player in players:
+        country = player['country']
+        position = player['position']
+        result.setdefault(country, {})
+        result[country].setdefault(position, [])
+        result[country][position].append(player)
+    return result
