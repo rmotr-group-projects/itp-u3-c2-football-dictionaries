76e8c13e225cc255b386c2cc41ed787d7b1c7eec
mhooke <mhooke@me.com>
2019-01-25 21:06:47 +0000

    Passing all tests

--- football_dictionaries/assignment_1.py
+++ football_dictionaries/assignment_1.py
@@ -1,2 +1,22 @@
+#from squads_data import SQUADS_DATA
+
 def players_as_dictionaries(squads_list):
-    pass
+    results = []
+    for squad in squads_list:
+        player = {}
+
+        player.setdefault('number', squad[0])
+        player.setdefault('position', squad[1])
+        player.setdefault('name', squad[2])
+        player.setdefault('date_of_birth', squad[3])
+        player.setdefault('caps', squad[4])
+        player.setdefault('club', squad[-4])
+        player.setdefault('country', squad[-3])
+        player.setdefault('club_country', squad[-2])
+        player.setdefault('year', squad[-1])
+        results.append(player)
+        
+    return (results)
+
+#x = players_as_dictionaries(SQUADS_DATA)
+#print(x)
--- football_dictionaries/assignment_3.py
+++ football_dictionaries/assignment_3.py
@@ -1,2 +1,28 @@
+#from squads_data import SQUADS_DATA
+
 def players_by_country_and_position(squads_list):
-    pass
+    results = {}
+    
+    for squad in squads_list:
+        player = {}
+
+        player.setdefault('number', squad[0])
+        player.setdefault('position', squad[1])
+        player.setdefault('name', squad[2])
+        player.setdefault('date_of_birth', squad[3])
+        player.setdefault('caps', squad[4])
+        player.setdefault('club', squad[-4])
+        player.setdefault('country', squad[-3])
+        player.setdefault('club_country', squad[-2])
+        player.setdefault('year', squad[-1])
+        
+        results.setdefault(player['country'], {})
+        results[player['country']].setdefault(player['position'], [])
+        results[player['country']][player['position']].append(player)
+
+    return results
+
+#x = players_by_country_and_position(SQUADS_DATA)
+#print(x)
+
+
