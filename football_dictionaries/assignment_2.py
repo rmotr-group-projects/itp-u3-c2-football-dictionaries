8d0cc278f78723877cdc85e0b22794d2d478cddb
knownhuman <pbradleyrobb@gmail.com>
2019-01-26 16:41:08 -0500

    All tests passing
    all tests passing uplading to github

--- football_dictionaries/assignment_2.py
+++ football_dictionaries/assignment_2.py
@@ -1,2 +1,10 @@
+from football_dictionaries.assignment_1 import players_as_dictionaries
+
 def players_by_position(squads_list):
-    pass
+    squad_dict = players_as_dictionaries(squads_list)
+    pos_dict = {}
+    for player in squad_dict:
+        pos = player.get('position')
+        pos_dict.setdefault(pos, [])
+        pos_dict[pos].append(player)
+    return pos_dict
--- football_dictionaries/assignment_3.py
+++ football_dictionaries/assignment_3.py
@@ -1,2 +1,13 @@
+from football_dictionaries.assignment_1 import players_as_dictionaries
+
 def players_by_country_and_position(squads_list):
-    pass
+    squad_dict = players_as_dictionaries(squads_list)
+    country_dict = {}
+    for player in squad_dict:
+        country = player.get('country')
+        position = player.get('position')
+        country_dict.setdefault(country, {})
+        country_dict[country].setdefault(position, [])
+        country_dict[country][position].append(player)
+
+    return country_dict
