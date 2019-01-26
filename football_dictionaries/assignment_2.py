3b1b740402a4bcb11ddecaaa02230a21c7d02d6d
blakepeeling <blake.peeling@gmail.com>
2019-01-26 00:25:04 +0000

    Completed all hw, tests passing

--- football_dictionaries/assignment_2.py
+++ football_dictionaries/assignment_2.py
@@ -1,2 +1,13 @@
+# Assignment 1
+from football_dictionaries.assignment_1 import players_as_dictionaries
+
 def players_by_position(squads_list):
-    pass
+    new_squad_list = players_as_dictionaries(squads_list)
+    by_position = {}
+    
+    for player in new_squad_list:
+        position = player['position']
+        by_position.setdefault(position, [])
+        by_position[position].append(player)
+
+    return by_position
