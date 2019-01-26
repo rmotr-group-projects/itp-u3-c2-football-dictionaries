02d88eb889d91a83d5c1a51cbf8ab81de15e95e7
vuntsova <evuntsova@gmail.com>
2019-01-26 23:16:57 +0000

    assignment 1 ready for test

--- football_dictionaries/assignment_1.py
+++ football_dictionaries/assignment_1.py
@@ -1,2 +1,14 @@
+from squads_data import SQUADS_DATA
+
 def players_as_dictionaries(squads_list):
-    pass
+    result=[]
+    list_names = [
+        'number', 'position', 'name', 'date_of_birth', 'caps', 'club', 'country', 'club_country', 'year']
+    for a_list in squads_list:
+        dictionary = dict(zip(list_names, a_list))
+        result.append(dictionary)
+    return result
+print(players_as_dictionaries(SQUADS_DATA))
+
+
+
