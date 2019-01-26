3c8f2d4e5b7071c8eeaf7eac90ce8071f809ef4d
atmc612 <atmc612@gmail.com>
2019-01-26 23:34:23 +0000

    all tests passed for this weeks activity and assignment ready to be merged

--- football_dictionaries/assignment_3.py
+++ football_dictionaries/assignment_3.py
@@ -1,2 +1,19 @@
 def players_by_country_and_position(squads_list):
-    pass
+    cnt_pos_dict = {}
+    for player in squads_list:
+        country = player[-3]
+        position = player[1]
+        cnt_pos_dict.setdefault(country,{})
+        cnt_pos_dict[country].setdefault(position, [])
+        cnt_pos_dict[country][position].append({
+            'number':player[0],
+            'position':player[1],
+            'name':player[2],
+            'date_of_birth':player[3],
+            'caps':player[4],
+            'club':player[5],
+            'country':player[6],
+            'club_country':player[7],
+            'year':player[8]
+        })
+    return cnt_pos_dict
