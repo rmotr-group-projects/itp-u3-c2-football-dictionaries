233c12f3a72428584c7ed6fb81f228907d3e929a
knownhuman <pbradleyrobb@gmail.com>
2019-01-27 09:48:06 -0500

    Update assignment_3.py

--- football_dictionaries/assignment_3.py
+++ football_dictionaries/assignment_3.py
@@ -3,7 +3,7 @@ from football_dictionaries.assignment_1 import players_as_dictionaries
 def players_by_country_and_position(squads_list):
     squad_dict = players_as_dictionaries(squads_list)
     country_dict = {}
-    for player in squad_dict():
+    for player in squad_dict:
         country = player.get('country')
         position = player.get('position')
         country_dict.setdefault(country, {})
