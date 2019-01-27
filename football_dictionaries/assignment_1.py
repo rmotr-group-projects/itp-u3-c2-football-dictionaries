66590882727ffa50b6d19cc7bec87e36417f3a19
hina-s <hina.11nov@gmail.com>
2019-01-27 22:57:45 +0000

    Passed all tests!

--- football_dictionaries/assignment_1.py
+++ football_dictionaries/assignment_1.py
@@ -1,2 +1,35 @@
+def transform_list_to_dict(squads_list):
+    squads_dict = {
+        'number' : squads_list[0],
+        'position' : squads_list[1],
+        'name' : squads_list[2],
+        'date_of_birth' : squads_list[3],
+        'caps' : squads_list[4],
+        'club' : squads_list[5],
+        'club_country' : squads_list[7],
+        'country' : squads_list[6],
+        'year' : squads_list[8],
+    }
+        
+    return squads_dict
+
 def players_as_dictionaries(squads_list):
-    pass
+    players_list = []
+    
+    for player in squads_list:
+ #call above function to append the dict to a list 
+        squads_dict = transform_list_to_dict(player)
+        players_list.append(squads_dict)
+        
+    return players_list    
+
+#from squads_data import SQUADS_DATA
+
+#print(players_as_dictionaries(SQUADS_DATA))    
+
+
+#from football_dictionaries.squads_data import SQUADS_DATA
+
+#def players_as_dictionaries(squads_list):
+ #   list_names = ['number', 'position', 'name', 'date_of_birth', 'caps', 'club', 'country', #'club_country', 'year']
+ #   return [dict(zip(list_names, a_list)) for a_list in squads_list]
--- football_dictionaries/assignment_3.py
+++ football_dictionaries/assignment_3.py
@@ -1,2 +1,25 @@
+from football_dictionaries.assignment_1 import players_as_dictionaries
+
+#from pprint import pprint 
+
 def players_by_country_and_position(squads_list):
-    pass
+    result = {}
+    
+    #importing dictionary from assignment_2
+    player_dict = players_as_dictionaries(squads_list)
+    
+    for player in player_dict:
+        country = player['country']  
+        position = player['position']
+        result.setdefault(country, {})
+        result[country].setdefault(position,[])
+        result[country][position].append(player)
+        
+    return result    
+
+
+#from squads_data import SQUADS_DATA
+#pprint(players_by_country_and_position(SQUADS_DATA))    
+        
+
+
