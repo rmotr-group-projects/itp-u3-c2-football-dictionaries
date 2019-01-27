073a1ab313d33641e79f2c6093213c5fa3e32e91
vuntsova <evuntsova@gmail.com>
2019-01-27 02:30:18 +0000

    ready for testing

--- football_dictionaries/assignment_1.py
+++ football_dictionaries/assignment_1.py
@@ -1,12 +1,4 @@
 from football_dictionaries.squads_data import SQUADS_DATA
-
-
 def players_as_dictionaries(squads_list):
-    result=[]
-    list_names = [
-        'number', 'position', 'name', 'date_of_birth', 'caps', 'club', 'country', 'club_country', 'year']
-    for a_list in squads_list:
-        dictionary = dict(zip(list_names, a_list))
-        result.append(dictionary)
-    return result
-# print(players_as_dictionaries(SQUADS_DATA))
+    list_names = ['number', 'position', 'name', 'date_of_birth', 'caps', 'club', 'country', 'club_country', 'year']
+    return [dict(zip(list_names, a_list)) for a_list in squads_list]
--- football_dictionaries/assignment_2.py
+++ football_dictionaries/assignment_2.py
@@ -3,17 +3,11 @@ from football_dictionaries.assignment_1 import players_as_dictionaries
 
 def players_by_position(squads_list):
     squash_dict = players_as_dictionaries(squads_list)
-    result={}
-    
+    result={}    
     
     for dict_ in squash_dict:
         position = dict_['position']
         result.setdefault(position, [])
         result[position].append(dict_)
     return result
-
-
-
-
-
 # print(players_by_position(SQUADS_DATA))
