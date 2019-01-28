23a2c0d4b5e6d31d0c63eef1250cde5c32b84b26
Deecreative <devon.thompson0@gmail.com>
2019-01-28 23:36:22 +0000

    type a message in here

--- football_dictionaries/assignment_1.py
+++ football_dictionaries/assignment_1.py
@@ -1,2 +1,23 @@
-def players_as_dictionaries(squads_list):
+from football_dictionaries.assignment_1 import players_as_dictionaries
+
+    def players_as_dictionaries(squads_list):
     pass
+
+    users = []
+        for user in squads_list:
+        user_dict = {
+            'number': user[0],
+            'position': user[1],
+            'name': user[2],
+            'date_of_birth': user[3],
+            'caps': user[4],
+            'club': user[5],
+            'country': user[6],
+            'club_country': user[7],
+            'year': user[8],
+        }
+        users.append(user_dict)
+    return users
+
+
+
--- football_dictionaries/assignment_2.py
+++ football_dictionaries/assignment_2.py
@@ -1,2 +1,36 @@
+    from football_dictionaries.assignment_2 import players_by_position
+
+    def players_as_dictionaries(squads_list):
+    users = []
+    for user in squads_list:
+        user_dict = {
+            'number': user[0],
+            'position': user[1],
+            'name': user[2],
+            'date_of_birth': user[3],
+            'caps': user[4],
+            'club': user[5],
+            'country': user[6],
+            'club_country': user[7],
+            'year': user[8],
+        }
+        users.append(user_dict)
+    return users
+
 def players_by_position(squads_list):
-    pass
+    #create empty dictionary
+    position_dict = {}
+    # run step 1s function
+    players = players_as_dictionaries(squads_list)
+    for player in players:
+        position = player['position'] 
+        position_dict.setdefault(position, [])
+        position_dict[position].append(player)
+        
+    return position_dict
+
+
+
+
+
+
