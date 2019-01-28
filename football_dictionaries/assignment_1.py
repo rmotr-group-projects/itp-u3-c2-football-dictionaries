a8797aacfa91b04d069da8c15d344ce620c7a0c9
csrich90 <csrich90@gmail.com>
2019-01-28 01:58:30 +0000

    football dict ready for sumbit

--- football_dictionaries/assignment_1.py
+++ football_dictionaries/assignment_1.py
@@ -1,2 +1,16 @@
 def players_as_dictionaries(squads_list):
-    pass
+    res = []
+    for player in squads_list:
+        temp ={
+        'caps': player[4],
+        'club': player[5],
+        'club_country': player[7],
+        'country': player[6],
+        'date_of_birth': player[3],
+        'name': player[2],
+        'number': player[0],
+        'position': player[1],
+        'year': player[8]
+        }
+        res.append(temp)
+    return res
