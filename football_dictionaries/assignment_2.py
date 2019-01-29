e460527ab660a6eb6002d36a59023ea6b4aabe65
ujwal90210 <ujwal90210@gmail.com>
2019-01-29 08:41:27 +0000

    completed the assignment

--- football_dictionaries/assignment_2.py
+++ football_dictionaries/assignment_2.py
@@ -1,2 +1,40 @@
 def players_by_position(squads_list):
-    pass
+    positions = {"GK": [], "MF" : [], "FW" : []}
+    for squad in squads_list:
+        if 'GK' in squad:
+            positions['GK'].append({
+                'caps': squad[4],
+                'club': squad[5],
+                'club_country': squad[6],
+                'country': squad[7],
+                'date_of_birth': squad[3],
+                'name': squad[2],
+                'number': squad[0],
+                'position': squad[1],
+                'year': squad[8]
+            })
+        elif 'MF' in squad:
+             positions['MF'].append({
+                'caps': squad[4],
+                'club': squad[5],
+                'club_country': squad[6],
+                'country': squad[7],
+                'date_of_birth': squad[3],
+                'name': squad[2],
+                'number': squad[0],
+                'position': squad[1],
+                'year': squad[8]
+            })
+        elif 'FW' in squad: 
+              positions['FW'].append({
+                'caps': squad[4],
+                'club': squad[5],
+                'club_country': squad[6],
+                'country': squad[7],
+                'date_of_birth': squad[3],
+                'name': squad[2],
+                'number': squad[0],
+                'position': squad[1],
+                'year': squad[8]
+            })
+    return positions
--- football_dictionaries/assignment_3.py
+++ football_dictionaries/assignment_3.py
@@ -1,2 +1,55 @@
 def players_by_country_and_position(squads_list):
-    pass
+    countries = { 'Argentina' : {}, 'Belgium' : {}, 'Brazil' : {}, 'South Korea' : {}}
+    for country in countries:
+        for squad in squads_list:
+            if country in squad and "GK" in squad:
+                countries[country].setdefault('GK', []).append({
+                    'caps': squad[4],
+                    'club': squad[5],
+                    'club_country': squad[7],
+                    'country': squad[6],
+                    'date_of_birth': squad[3],
+                    'name': squad[2],
+                    'number': squad[0],
+                    'position': squad[1],
+                    'year': squad[8]
+                })
+            elif country in squad and "MF" in squad:
+                countries[country].setdefault('MF', []).append({
+                    'caps': squad[4],
+                    'club': squad[5],
+                    'club_country': squad[7],
+                    'country': squad[6],
+                    'date_of_birth': squad[3],
+                    'name': squad[2],
+                    'number': squad[0],
+                    'position': squad[1],
+                    'year': squad[8]
+                })
+            elif country in squad and "DF" in squad:
+                countries[country].setdefault("DF", []).append({
+                    'caps': squad[4],
+                    'club': squad[5],
+                    'club_country': squad[7],
+                    'country': squad[6],
+                    'date_of_birth': squad[3],
+                    'name': squad[2],
+                    'number': squad[0],
+                    'position': squad[1],
+                    'year': squad[8]
+                })
+            elif country in squad and "FW" in squad:
+                countries[country].setdefault("FW" , []).append({
+                    'caps': squad[4],
+                    'club': squad[5],
+                    'club_country': squad[7],
+                    'country': squad[6],
+                    'date_of_birth': squad[3],
+                    'name': squad[2],
+                    'number': squad[0],
+                    'position': squad[1],
+                    'year': squad[8]
+                })
+
+            
+    return countries
