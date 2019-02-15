b54a858e416a74e169a4eac3faba3758b31ee218
Loan Tran <loantranvn810@gmail.com>
2019-02-15 13:31:29 -0800

    Add assignment 2 and 3

--- football_dictionaries/assignment_2.py
+++ football_dictionaries/assignment_2.py
@@ -1,2 +1,9 @@
+from assignment_1 import players_as_dictionaries
+
 def players_by_position(squads_list):
-    pass
+    squads_dict = players_as_dictionaries(squads_list)
+    by_position = {}
+    for player in squads_dict:
+        by_position.setdefault(player['position'], []) 
+        by_position[player['position']].append(player)
+    return by_position 
