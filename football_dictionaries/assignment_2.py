d70e4c1c36ab37666b005f272d502e8220044541
Jonathan Flowers <jmflowers134@gmail.com>
2019-02-17 14:21:44 -0500

    Add files via upload

--- football_dictionaries/assignment_2.py
+++ football_dictionaries/assignment_2.py
@@ -1,2 +1,8 @@
-def players_by_position(squads_list):
-    pass
+def players_by_position(squad_data):
+    arr = {}
+    for person in squad_data:
+        if person['position'] not in arr:
+            arr[person['position']]=[person]
+        else:
+            arr[person['position']].append(person)
+    return arr
--- football_dictionaries/assignment_3.py
+++ football_dictionaries/assignment_3.py
@@ -1,2 +1,15 @@
-def players_by_country_and_position(squads_list):
-    pass
+def players_by_country_and_position(squad_data):
+    posArr = {'GK':[], 'FW':[], 'DF':[], 'MF':[]}
+    arr = {}
+    for person in squad_data:
+        if person['country'] not in arr:
+            arr[person['country']]=posArr
+        if person['position']=='GK':
+            arr[person['country']]['GK'].append(person)
+        elif person['position']=='FW':
+            arr[person['country']]['FW'].append(person)
+        elif person['position']=='DF':
+            arr[person['country']]['DF'].append(person)
+        elif person['position']=='MF':
+            arr[person['country']]['MF'].append(person)    
+    return arr
