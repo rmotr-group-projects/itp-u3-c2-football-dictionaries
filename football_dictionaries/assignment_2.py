97797a93614e6aedcd39ef4587ebcbeb2a4437a8
Jonathan Flowers <jmflowers134@gmail.com>
2019-02-20 17:36:25 -0500

    Delete assignment_2.py

--- football_dictionaries/assignment_2.py
+++
@@ -1,8 +0,0 @@
-def players_by_position(squad_data):
-    arr = {}
-    for person in squad_data:
-        if person['position'] not in arr:
-            arr[person['position']]=[person]
-        else:
-            arr[person['position']].append(person)
-    return arr
