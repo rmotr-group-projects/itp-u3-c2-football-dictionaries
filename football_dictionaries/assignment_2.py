13cda596f6c38e0e94dafdf36b7f3c3fe1e4c662
Jonathan Flowers <jmflowers134@gmail.com>
2019-02-21 22:36:00 -0500

    Update assignment_2.py

--- football_dictionaries/assignment_2.py
+++ football_dictionaries/assignment_2.py
@@ -1,17 +1,3 @@
-import csv
-import os
-
-def read_squad_file(file_name='test_data.csv'):
-    squad = []
-    with open(file_name) as fp:
-        reader = csv.reader(fp)
-        for line in reader:
-            squad.append(line)
-
-    return squad
-
-squad = read_squad_file(file_name='test_data.csv')
-
 def players_as_dictionaries(players):
     retArr=[]
     for player in players:
@@ -28,16 +14,12 @@ def players_as_dictionaries(players):
             })
     return retArr
 
-squad_data_test = players_as_dictionaries(squad)
-
 def players_by_position(squad_data):
+    squad = players_as_dictionaries(squad_data)
     arr = {}
-    for person in squad_data:
+    for person in squad:
         if person['position'] not in arr:
             arr[person['position']]=[person]
         else:
             arr[person['position']].append(person)
     return arr
-
-position_dict = players_by_position(squad_data_test)
-print(position_dict)
