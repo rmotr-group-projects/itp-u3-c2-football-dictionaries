787223caa87075555e448e713baca5105d6c3205
doulfa <amadoulfa85@gmail.com>
2019-02-25 04:15:50 +0000

    updated football_dictionaries in order to pass all tests

--- football_dictionaries/assignment_1.py
+++ football_dictionaries/assignment_1.py
@@ -1,2 +1,21 @@
+def transform_single_list_to_dic(player_list):
+    player_dic = {
+        'caps': player_list[4],
+        'club': player_list[5],
+        'club_country': player_list[7],
+        'country': player_list[6],
+        'date_of_birth': player_list[3],
+        'name': player_list[2],
+        'number': player_list[0],
+        'position': player_list[1],
+        'year': player_list[8]
+    }
+    return player_dic 
+
 def players_as_dictionaries(squads_list):
-    pass
+    list_of_player_dic =[]
+    for player in squads_list:
+        dic_player = transform_single_list_to_dic(player)
+        list_of_player_dic.append(dic_player)
+    return list_of_player_dic
+    
--- football_dictionaries/assignment_2.py
+++ football_dictionaries/assignment_2.py
@@ -1,2 +1,15 @@
+from football_dictionaries.assignment_1 import transform_single_list_to_dic
+
+def get_player_position(single_list):
+    return single_list[1]
+
 def players_by_position(squads_list):
-    pass
+    player_dic_by_positon = {}
+    
+    for player in squads_list:
+        position = get_player_position(player)
+        player_dic_by_positon.setdefault(position,[])
+        player_dic = transform_single_list_to_dic(player)
+        player_dic_by_positon[position].append(player_dic)
+    
+    return player_dic_by_positon
