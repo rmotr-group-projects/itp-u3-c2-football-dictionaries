e614801d344c6ff47c65ea0c496522f82ca813d9
thaodang138 <dangthuthao138@gmail.com>
2019-02-25 23:44:20 +0000

    Updated football dictionaries in order to pass all tests

--- football_dictionaries/assignment_1.py
+++ football_dictionaries/assignment_1.py
@@ -1,2 +1,18 @@
+from football_dictionaries.squads_data import SQUADS_DATA
 def players_as_dictionaries(squads_list):
-    pass
+    players_list = []
+    for player in squads_list:    
+        players_dictionaries = {
+            'caps': player[4],
+            'club': player[-4],
+            'club_country': player[-2],
+            'country': player[-3],
+            'date_of_birth': player[3],
+            'name': player[2],
+            'number': player[0],
+            'position': player[1],
+            'year': player[-1],
+            }
+        players_list.append(players_dictionaries)
+        
+    return players_list
