04bb2a41c654735a4e21d907d0e91e83522ef2c0
hllywdnghts <ho.entertainmentjunk@gmail.com>
2019-03-02 01:52:56 +0000

    Harrison Obeid Football Dictionaries

--- football_dictionaries/assignment_2.py
+++ football_dictionaries/assignment_2.py
@@ -1,2 +1,25 @@
 def players_by_position(squads_list):
-    pass
+    position_dict = {
+        'GK': [],
+        'MF': [],
+        'FW': []
+    }
+    
+    for player in squads_list:
+        entry = {
+            'caps': player[4],
+            'club': player[5],
+            'club_country': player[6],
+            'country': player[7],
+            'date_of_birth': player[3],
+            'name': player[2],
+            'number': player[0],
+            'position': player[1],
+            'year': player[8]
+        }
+        
+        position = entry['position']
+        position_dict[position].append(entry)
+   
+    
+    return position_dict
