206415794c87919dbe313583650f0012b329604f
Nicklas Burwell <Drol_Tromedlov@yahoo.com>
2019-03-07 21:46:58 -0500

    Football dictionaries finished

--- football_dictionaries/assignment_2.py
+++ football_dictionaries/assignment_2.py
@@ -1,2 +1,13 @@
+from football_dictionaries.assignment_1 import players_as_dictionaries
+from football_dictionaries.assignment_1 import transform_list_player_to_dict
+
+
 def players_by_position(squads_list):
-    pass
+    positions_dict = {}
+    for player in players_as_dictionaries(squads_list):
+        if player["position"] not in positions_dict.keys():
+            positions_dict.setdefault(player['position'], [player])
+        else:
+            positions_dict[player["position"]].append(player)
+
+    return positions_dict
--- football_dictionaries/assignment_3.py
+++ football_dictionaries/assignment_3.py
@@ -1,2 +1,13 @@
+from football_dictionaries.assignment_1 import players_as_dictionaries
+
+
 def players_by_country_and_position(squads_list):
-    pass
+    country_dict = {}
+    for player in players_as_dictionaries(squads_list):
+        if player['country'] not in country_dict.keys():
+            country_dict.setdefault(player['country'], {})
+        if player["position"] not in country_dict[player['country']]:
+            country_dict[player['country']].setdefault(player['position'], [player])
+        else:
+            country_dict[player['country']][player["position"]].append(player)
+    return country_dict
