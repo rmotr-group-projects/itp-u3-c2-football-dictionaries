f86735fc05f13d7950235dae198156e4ecef0fca
vchau76 <vchau76@gmail.com>
2019-03-13 22:41:26 +0000

    Completed assignment files for Football Dictionaries

--- football_dictionaries/assignment_2.py
+++ football_dictionaries/assignment_2.py
@@ -1,2 +1,10 @@
+from football_dictionaries.assignment_1 import players_as_dictionaries
+
 def players_by_position(squads_list):
-    pass
+    players = players_as_dictionaries(squads_list)
+    players_dict = {}
+    for player in players:
+        position = player['position']
+        players_dict.setdefault(position,[])
+        players_dict[position].append(player)
+    return players_dict
--- football_dictionaries/assignment_3.py
+++ football_dictionaries/assignment_3.py
@@ -1,2 +1,30 @@
+from football_dictionaries.assignment_1 import players_as_dictionaries
+from football_dictionaries.assignment_2 import players_by_position
+'''
+Dict > Dict > List > Dict
+{
+  "Argentina": {
+    "GK": [{..player1..}, {..player2..}],
+    "DF": [{..player1..}, {..player2..}],
+    "MF": [{..player1..}, {..player2..}],
+    "FW": [{..player1..}, {..player2..}],
+  },
+  "Brazil": {
+    "GK": [{..player1..}, {..player2..}],
+    "DF": [{..player1..}, {..player2..}],
+    "MF": [{..player1..}, {..player2..}],
+    "FW": [{..player1..}, {..player2..}],
+  }
+}
+'''
 def players_by_country_and_position(squads_list):
-    pass
+    ppositions = players_as_dictionaries(squads_list)
+    player_country = {}
+    for player in ppositions:
+        country = player['country']
+        position = player['position']
+        player_country.setdefault(country,{})
+        player_country[country].setdefault(position,[])
+        player_country[country][position].append(player)
+        
+    return player_country
