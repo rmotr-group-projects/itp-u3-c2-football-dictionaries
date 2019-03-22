8e81f13ff9db7e03ab30fbf5d81f0c482e848a36
gmrkss <parlamasgeorge@gmail.com>
2019-03-22 04:51:21 +0000

    assignments 1, 2 and warm up passed tests.

--- football_dictionaries/assignment_2.py
+++ football_dictionaries/assignment_2.py
@@ -1,2 +1,17 @@
+from football_dictionaries.assignment_1 import players_as_dictionaries
+
 def players_by_position(squads_list):
-    pass
+    result = {
+        'GK' : [],
+        'MF' : [],
+        'FW' : [],
+        }
+    p_dic = players_as_dictionaries(squads_list)
+    for player in p_dic:
+        if player['position'] == 'GK':
+            result['GK'].append(player)
+        if player['position'] == 'MF':
+            result['MF'].append(player)
+        if player['position'] == 'FW':
+            result['FW'].append(player)
+    return result
