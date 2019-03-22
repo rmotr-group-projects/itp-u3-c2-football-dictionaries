be5368146d76f36a95716bf635b3e2fadbcaeb72
vidsjai <jaiganeshd@gmail.com>
2019-03-22 10:46:12 +0000

    changes to assignment_1

--- football_dictionaries/assignment_1.py
+++ football_dictionaries/assignment_1.py
@@ -1,2 +1,14 @@
 def players_as_dictionaries(squads_list):
-    pass
+    SQUADS_DATA={}
+    
+    SQUADS_DATA['number']=squads_list[0],
+    SQUADS_DATA['position']=squads_list[1],
+    SQUADS_DATA['name']=squads_list[2],
+    SQUADS_DATA['date_of_birth']=squads_list[3],
+    SQUADS_DATA['caps']=squads_list[4],
+    SQUADS_DATA['club']=squads_list[5],
+    SQUADS_DATA['country']=squads_list[6],
+    SQUADS_DATA['club_country']=squads_list[7],
+    SQUADS_DATA['year']=squads_list[8]
+    return SQUADS_DATA
+    
