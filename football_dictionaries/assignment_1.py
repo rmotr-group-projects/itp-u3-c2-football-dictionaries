7267839fb23cb2229d130a21035f4a751b63ba15
vidsjai <jaiganeshd@gmail.com>
2019-03-23 10:38:10 +0000

    changed assignment_1.py

--- football_dictionaries/assignment_1.py
+++ football_dictionaries/assignment_1.py
@@ -1,14 +1,19 @@
+from football_dictionaries import squads_data
+
 def players_as_dictionaries(squads_list):
-    SQUADS_DATA={}
-    
-    SQUADS_DATA['number']=squads_list[0],
-    SQUADS_DATA['position']=squads_list[1],
-    SQUADS_DATA['name']=squads_list[2],
-    SQUADS_DATA['date_of_birth']=squads_list[3],
-    SQUADS_DATA['caps']=squads_list[4],
-    SQUADS_DATA['club']=squads_list[5],
-    SQUADS_DATA['country']=squads_list[6],
-    SQUADS_DATA['club_country']=squads_list[7],
-    SQUADS_DATA['year']=squads_list[8]
-    return SQUADS_DATA
-    
+    users = []
+    for user in squads_list:
+        user_dict = {
+
+            'number': user[0],
+            'position': user[1],
+            'name': user[2],
+            'date_of_birth': user[3],
+            'caps': user[4],
+            'club': user[5],
+            'country': user[6],
+            'club_country': user[7],
+            'year': user[8],
+        }
+        users.append(user_dict)
+    return users
