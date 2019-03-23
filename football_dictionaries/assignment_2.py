0a770a5128a37c6130631540c5baee362ae32816
vidsjai <jaiganeshd@gmail.com>
2019-03-23 10:57:27 +0000

    changed assignment_2.py

--- football_dictionaries/assignment_2.py
+++ football_dictionaries/assignment_2.py
@@ -1,2 +1,15 @@
+from football_dictionaries import squads_data
+from football_dictionaries.assignment_1 import players_as_dictionaries
+
 def players_by_position(squads_list):
-    pass
+    position_dict={}
+    squads_data=players_as_dictionaries(squads_list)
+    for squad_pos in squads_data:
+        position=squad_pos['position']
+        position_dict.setdefault(position,[])
+        position_dict[position].append(squad_pos)
+    return position_dict
+            
+                
+        
+    
