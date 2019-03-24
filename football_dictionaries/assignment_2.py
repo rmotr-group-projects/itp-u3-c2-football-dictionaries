a804806ab1cacc7f2db144201d3b2647c98fbbaa
selmabensaid <selmabensaid@hotmail.com>
2019-03-24 04:24:19 +0000

    football dictionnaries passed all tests

--- football_dictionaries/assignment_2.py
+++ football_dictionaries/assignment_2.py
@@ -1,2 +1,10 @@
+from football_dictionaries.assignment_1 import players_as_dictionaries
+
 def players_by_position(squads_list):
-    pass
+    players_by_position_dict={}
+    players_dict=players_as_dictionaries(squads_list)
+    for player in players_dict:
+        players_by_position_dict.setdefault(player['position'],[])
+        players_by_position_dict[player['position']].append(player)
+    return players_by_position_dict    
+        
--- football_dictionaries/assignment_3.py
+++ football_dictionaries/assignment_3.py
@@ -1,2 +1,20 @@
+# Assignment 1
+from football_dictionaries.assignment_1 import players_as_dictionaries
+# Assignment 2
+from football_dictionaries.assignment_2 import players_by_position
+
 def players_by_country_and_position(squads_list):
-    pass
+    players_dict = players_as_dictionaries(squads_list)
+    players_by_country={}
+    player_by_country_and_position={}
+    for player in players_dict:
+        players_by_country.setdefault(player['country'],[])
+        players_by_country[player['country']].append(player)
+    for country,country_list in players_by_country.items():
+        player_by_country_and_position.setdefault(country,{})
+        for player in country_list:
+            player_by_country_and_position[country].setdefault(player['position'],[])
+            player_by_country_and_position[country][player['position']].append(player)
+    return player_by_country_and_position    
+        
+        
