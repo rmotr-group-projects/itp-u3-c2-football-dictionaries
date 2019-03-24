d3c920cc1d4f4ec49a718b7198e710ce3a4a45d6
spacecowgirl <samantha.walsh@gmail.com>
2019-03-24 16:40:48 +0000

    Football dictionaries project. Passing all tests.

--- football_dictionaries/assignment_3.py
+++ football_dictionaries/assignment_3.py
@@ -1,2 +1,18 @@
 def players_by_country_and_position(squads_list):
-    pass
+    countries = {}
+    for player in squads_list:
+        countries.setdefault(player[6], {}) 
+        countries[player[6]].setdefault(player[1], [])
+        player_dict = {
+            'number': player[0],
+            'position': player[1],
+            'name': player[2],
+            'date_of_birth': player[3],
+            'caps': player[4],
+            'club': player[5],
+            'country': player[6],
+            'club_country': player[7],
+            'year': player[8],
+        }    
+        countries[player[6]][player[1]].append(player_dict)
+    return countries
