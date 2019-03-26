3b7a5136efc15a7dc9c2359c5d0c1ba45665b1ac
gmrkss <parlamasgeorge@gmail.com>
2019-03-26 21:48:20 +0000

    Passed all tests, for submission

--- football_dictionaries/assignment_1.py
+++ football_dictionaries/assignment_1.py
@@ -4,13 +4,13 @@ def players_as_dictionaries(squads_list):
         p_dic = {
             'caps' : player[4],
             'club' : player[5],
-            'club_country' : player[6],
-            'country' : player[7],
+            'club_country' : player[7],
+            'country' : player[6],
             'date_of_birth' : player[3],
             'name' : player[2],
             'number' : player[0],
             'position' : player[1],
-            'year' : player[-1]
+            'year' : player[8]
         }
         result.append(p_dic)
     return result
--- football_dictionaries/assignment_3.py
+++ football_dictionaries/assignment_3.py
@@ -1,2 +1,36 @@
+from football_dictionaries.assignment_1 import players_as_dictionaries
+
 def players_by_country_and_position(squads_list):
-    pass
+    player_dict = players_as_dictionaries(squads_list)
+    
+    result={}
+    
+    for player in player_dict:
+        country=player['country']
+        position=player['position']
+        result.setdefault(country, {})
+        result[country].setdefault(position,[])
+        result[country][position].append(player)
+        
+    return result
+
+#     result={
+#         'Argentina' : [],
+#         'Belgium' : [],
+#         'Brazil' : [],
+#         'South Korea' : []
+#     }
+
+#     for position in pos_dic:
+#         for i in range(len(position)):
+#             if pos_dic[position][i]['country']=='Argentina':
+#                 result['Argentina'].append({position: pos_dic[position]})
+#             if pos_dic[position][i]['country']=='Belgium':
+#                 result['Belgium'].append({position: pos_dic[position]})
+#             if pos_dic[position][i]['country']=='Brazil':
+#                 result['Brazil'].append({position: pos_dic[position]})
+#             if pos_dic[position][i]['country']=='South Korea':
+#                 result['South Korea'].append({position: pos_dic[position]})
+
+    return result
+        
