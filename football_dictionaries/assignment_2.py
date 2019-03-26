99901b0dc5f44eb119f57d6ffafa7ce22f8e48c2
maralegui <mara.leguizamon@gmail.com>
2019-03-26 00:54:23 +0000

    i couldn´t solve assig 3.  The list doesn´t filter the country

--- football_dictionaries/assignment_2.py
+++ football_dictionaries/assignment_2.py
@@ -1,2 +1,10 @@
+from football_dictionaries.assignment_1 import players_as_dictionaries
+
 def players_by_position(squads_list):
-    pass
+    pos_players = {}
+    list_dic_players = players_as_dictionaries(squads_list)
+    for player in list_dic_players:
+        position = player["position"]
+        pos_players.setdefault(position,[])
+        pos_players[position].append(player)
+    return pos_players
--- football_dictionaries/assignment_3.py
+++ football_dictionaries/assignment_3.py
@@ -1,2 +1,15 @@
+from football_dictionaries.assignment_2 import players_by_position
+
 def players_by_country_and_position(squads_list):
-    pass
+    pais_players = {}
+    lista = {}
+    pos_players = players_by_position(squads_list)
+    for pos,players in pos_players.items(): 
+        lista.setdefault(pos,[])
+        for player in players:
+            pais = player["country"]
+            pais_players.setdefault(pais,lista)
+            pais_players[pais][pos].append(player)
+    print(pais_players)
+    return pais_players
+    
