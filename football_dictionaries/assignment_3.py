17e4138ccc8226d46273662f188c92e5e6ab2fb8
maralegui <mara.leguizamon@gmail.com>
2019-03-26 22:57:26 +0000

    All solved

--- football_dictionaries/assignment_3.py
+++ football_dictionaries/assignment_3.py
@@ -2,18 +2,15 @@ from football_dictionaries.assignment_2 import players_by_position
 
 def players_by_country_and_position(squads_list):
     pais_players = {}
-    lista ={}
+    posiciones_dic = {}
     pos_players = players_by_position(squads_list)
     for pos,players in pos_players.items(): 
         for player in players:
             pais = player["country"]
             pais_players.setdefault(pais,{})
-    for p in list(pais_players.keys()):
-        for pos,players in pos_players.items(): 
-            for player in players:
-                if p == player["country"]:
-                    pais_players[p]=lista.setdefault(pos,[])
-                    pais_players[p]= lista 
-                    lista[pos].append(player)
+            for key_pais, dic_pos in pais_players.items():
+                if pais == key_pais:
+                    pais_players[pais].setdefault(pos,[])
+                    pais_players[pais][pos].append(player)        
     return pais_players
     
