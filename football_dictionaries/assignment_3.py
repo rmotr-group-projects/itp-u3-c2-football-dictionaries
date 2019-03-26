24b44b5f8234b2954766fafa93cb22f024609547
maralegui <mara.leguizamon@gmail.com>
2019-03-26 14:59:18 +0000

    unsolved ass 3. No funciona el filtro por país

--- football_dictionaries/assignment_3.py
+++ football_dictionaries/assignment_3.py
@@ -2,14 +2,18 @@ from football_dictionaries.assignment_2 import players_by_position
 
 def players_by_country_and_position(squads_list):
     pais_players = {}
-    lista = {}
+    lista ={}
     pos_players = players_by_position(squads_list)
     for pos,players in pos_players.items(): 
-        lista.setdefault(pos,[])
         for player in players:
             pais = player["country"]
-            pais_players.setdefault(pais,lista)
-            pais_players[pais][pos].append(player)
-    print(pais_players)
+            pais_players.setdefault(pais,{})
+    for p in list(pais_players.keys()):
+        for pos,players in pos_players.items(): 
+            for player in players:
+                if p == player["country"]:
+                    pais_players[p]=lista.setdefault(pos,[])
+                    pais_players[p]= lista 
+                    lista[pos].append(player)
     return pais_players
     
