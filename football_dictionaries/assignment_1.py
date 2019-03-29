02084cfcda77a2c7fb8c52c46daba0ea6d9e886b
shellbell22 <morris.shelley@gmail.com>
2019-03-29 01:09:44 +0000

    passed all 3 tests

--- football_dictionaries/assignment_1.py
+++ football_dictionaries/assignment_1.py
@@ -1,2 +1,18 @@
 def players_as_dictionaries(squads_list):
-    pass
+    resultlist = []
+    for squad in squads_list:
+        playerdict = {
+        'caps': squad[4],
+        'club': squad[5],
+        'club_country': squad[7],
+        'country': squad[6],
+        'date_of_birth': squad[3],
+        'name': squad[2],
+        'number': squad[0],
+        'position': squad[1],
+        'year': squad[8]
+        }
+        resultlist.append(playerdict)
+    return resultlist
+    
+   
--- football_dictionaries/assignment_2.py
+++ football_dictionaries/assignment_2.py
@@ -1,2 +1,29 @@
+def players_as_dictionaries(squads_list):
+    resultlist = []
+    for squad in squads_list:
+        playerdict = {
+        'caps': squad[4],
+        'club': squad[5],
+        'club_country': squad[7],
+        'country': squad[6],
+        'date_of_birth': squad[3],
+        'name': squad[2],
+        'number': squad[0],
+        'position': squad[1],
+        'year': squad[8]
+        }
+        resultlist.append(playerdict)
+    return resultlist
+    
+   
+
 def players_by_position(squads_list):
-    pass
+    ourlist = players_as_dictionaries(squads_list)
+    
+    result_dict = {}
+    
+    for squad in ourlist:
+        position = squad['position']
+        result_dict.setdefault(position, [])
+        result_dict[position].append(squad)
+    return result_dict
