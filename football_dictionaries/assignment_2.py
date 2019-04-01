43829b5cc057d5f2853635eb1d91a79759749ea1
maegankemp <maegankemp@outlook.com>
2019-04-01 23:21:22 +0000

    Passing all tests

--- football_dictionaries/assignment_2.py
+++ football_dictionaries/assignment_2.py
@@ -1,2 +1,15 @@
+        
+
 def players_by_position(squads_list):
-    pass
+    players = {}
+
+    for player_as_list in squads_list:
+        player = transform_list_player_to_dict(player_as_list)
+        position = player['position'] 
+        
+        players.setdefault(position, [])
+            
+        list_by_position = players.get(position)
+        list_by_position.append(player)
+    
+    return players
--- football_dictionaries/assignment_3.py
+++ football_dictionaries/assignment_3.py
@@ -1,2 +1,17 @@
+
+
 def players_by_country_and_position(squads_list):
-    pass
+    players_dict = players_as_dictionaries(squads_list)
+    players_by_country ={}
+    player_by_country_and_position = {} 
+
+    for player in players_dict:
+        players_by_country.setdefault(player['country'],[])
+        players_by_country[player['country']].append(player)
+    for country_in_country_list in players_by_country.items():
+        player_by_country_and_position.setdefault(country,{})
+        for player in country_in_country_list:
+            player_by_country_and_position[country].setdefault(player['position'],[])
+            player_by_country_and_position[country[player['position']].append(player)]
+    
+    return player_by_country_and_position
