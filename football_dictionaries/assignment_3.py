e47f6a081950242092b748e4de68eda0bf2b205e
maegankemp <maegankemp@outlook.com>
2019-04-08 22:38:30 +0000

    passing all tests

--- football_dictionaries/assignment_3.py
+++ football_dictionaries/assignment_3.py
@@ -1,19 +1,47 @@
+def players_as_dictionaries(squads_list):
+    players = []
+    for player in squads_list:
+        player_dict = {
+            'caps': player[4],
+            'club': player[5],
+            'club_country': player[7],
+            'country': player[6],
+            'date_of_birth': player[3],
+            'name': player[2],
+            'number': player[0],
+            'position': player[1],
+            'year': player[-1]
+            }
+        
+        players.append(player_dict)
+    return players
 
-from .assignment_1 import players_as_dictionaries
 
 
-def players_by_country_and_position(squads_list):
-    players_dict = players_as_dictionaries(squads_list)
-    players_by_country ={}
-    player_by_country_and_position = {} 
+from football_dictionaries.assignment_1 import players_as_dictionaries
 
-    for player in players_dict:
-        players_by_country.setdefault(player['country'],[])
-        players_by_country[player['country']].append(player)
-    for country_in_country_list in players_by_country.items():
-        player_by_country_and_position.setdefault(country,{})
-        for player in country_in_country_list:
-            player_by_country_and_position[country].setdefault(player['position'],[])
-            player_by_country_and_position[country[player['position']].append(player)]
+def players_by_position(squad):
+    result = {}
+    players_list = players_as_dictionaries(squad)
+    for player in players_list:
+        pos = player['position']
+        result.setdefault(pos, [])
+        result[pos].append(player)
+    return result
+
+from football_dictionaries.assignment_1 import players_as_dictionaries
+
+
+def players_by_country_and_position(squad):
+    players = players_as_dictionaries(squad)
+    result = {}
+
+    for player in players:
+        pos = player['position']
+        country = player['country']
+        
+        result.setdefault(country, {})
+        result[country].setdefault(pos, [])
+        result[country][pos].append(player)
     
-    return player_by_country_and_position
+    return result
