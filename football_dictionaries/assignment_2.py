a3be556f5dad201a75d2499af8d06138877b0b24
maegankemp <maegankemp@outlook.com>
2019-04-08 22:09:02 +0000

    Passing all tests

--- football_dictionaries/assignment_2.py
+++ football_dictionaries/assignment_2.py
@@ -1,15 +1,10 @@
-from .assignment_1 import players_as_dictionaries
+from football_dictionaries.assignment_1 import players_as_dictionaries
 
-def players_by_position(squads_list):
-    players = {}
-
-    for player_as_list in squads_list:
-        player = transform_list_player_to_dict(player_as_list)
-        position = player['position'] 
-        
-        players.setdefault(position, [])
-            
-        list_by_position = players.get(position)
-        list_by_position.append(player)
-    
-    return players
+def players_by_position(squad):
+    result = {}
+    players_list = players_as_dictionaries(squad)
+    for player in players_list:
+        pos = player['position']
+        result.setdefault(pos, [])
+        result[pos].append(player)
+    return result
