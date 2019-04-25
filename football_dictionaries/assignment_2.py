b3e9a6877fc7f81b7c237d0d9da293729c49c4e1
facubidegain <facundo@facundoferro.dev>
2019-04-25 18:17:19 +0000

    solution

--- football_dictionaries/assignment_2.py
+++ football_dictionaries/assignment_2.py
@@ -1,2 +1,12 @@
+from football_dictionaries.assignment_1 import players_as_dictionaries
+
 def players_by_position(squads_list):
-    pass
+    squads_list = players_as_dictionaries(squads_list)
+    dict = {}
+    for player in squads_list:
+        position = player['position']
+        dict.setdefault(position,[])
+        dict[position].append(player)
+    return dict
+
+    
--- football_dictionaries/assignment_3.py
+++ football_dictionaries/assignment_3.py
@@ -1,2 +1,12 @@
+from football_dictionaries.assignment_1 import players_as_dictionaries
+
 def players_by_country_and_position(squads_list):
-    pass
+    squads_list = players_as_dictionaries(squads_list)
+    players = {}
+    for player in squads_list:
+        country = player['country']
+        position = player['position']
+        players.setdefault(country,{})
+        players[country].setdefault(position,[])
+        players[country][position].append(player)
+    return players
