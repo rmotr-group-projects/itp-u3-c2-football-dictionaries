6a7c7540760a599356661195be381c8d41dc938f
apdsrocha <apdsrocha@gmail.com>
2019-04-28 19:30:51 +0000

    solved assignment_2 - passing tests

--- football_dictionaries/assignment_2.py
+++ football_dictionaries/assignment_2.py
@@ -1,2 +1,14 @@
-def players_by_position(squads_list):
-    pass
+from pprint import pprint
+from football_dictionaries.assignment_1 import players_as_dictionaries
+
+def players_by_position(result):
+    result_dict = players_as_dictionaries(result)
+    final_answer = {}
+    for item in result_dict:
+        position_key = item.get('position')
+        final_answer.setdefault(position_key, [])
+        final_answer[position_key].append(item)
+    return final_answer
+
+
+
