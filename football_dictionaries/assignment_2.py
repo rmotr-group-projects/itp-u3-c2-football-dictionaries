7246cef37743a35e9a373691e6c1cc0c666f5cc8
naakaisa <naakaisa@gmail.com>
2019-04-28 18:02:44 +0000

    Add assignment solutions

--- football_dictionaries/assignment_2.py
+++ football_dictionaries/assignment_2.py
@@ -1,2 +1,12 @@
+from .assignment_1 import players_as_dictionaries
+
 def players_by_position(squads_list):
-    pass
+    player_squad = players_as_dictionaries(squads_list)   # from assignment 1
+    p_position = {}
+
+    for player in player_squad:
+        position = player['position']
+        p_position.setdefault(position, [])
+        p_position[position].append(player)
+
+    return p_position
