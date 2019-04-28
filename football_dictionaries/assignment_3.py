ba0f0f04bf9f452a2dd3c1026b6d54c0f3230b6b
tlinc02 <thomas.sgeorge@outlook.com>
2019-04-28 19:09:01 +0000

    Passing all assignment tests

--- football_dictionaries/assignment_3.py
+++ football_dictionaries/assignment_3.py
@@ -1,2 +1,13 @@
+from .assignment_1 import players_as_dictionaries
+
 def players_by_country_and_position(squads_list):
-    pass
+    results = {}
+    squads_data = players_as_dictionaries(squads_list)
+    for player in squads_data:
+        country = player['country']
+        position = player['position']
+        results.setdefault(country, {})
+        results[country].setdefault(position, [])
+        results[country][position].append(player)
+    
+    return results
