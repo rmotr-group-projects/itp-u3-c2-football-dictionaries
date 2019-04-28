1aec6932833991d89e56aec5d39262bf980b776a
Jing Liu <jingliu.922@gmail.com>
2019-04-27 19:42:51 -0700

    add finished homework

--- football_dictionaries/assignment_1.py
+++ football_dictionaries/assignment_1.py
@@ -1,2 +1,24 @@
 def players_as_dictionaries(squads_list):
-    pass
+    
+    tot_list=[]
+    
+    
+    
+    for elem in squads_list:
+        
+        
+        a_dict={}
+        
+        a_dict['caps']=elem[4] 
+        a_dict['club']=elem[5]
+        a_dict['club_country']=elem[7]
+        a_dict['country']=elem[6]
+        a_dict['date_of_birth']=elem[3]
+        a_dict['name']=elem[2]
+        a_dict['number']=elem[0]
+        a_dict['position']=elem[1]
+        a_dict['year']=elem[8]
+        
+        tot_list.append(a_dict)
+    
+    return tot_list
--- football_dictionaries/assignment_2.py
+++ football_dictionaries/assignment_2.py
@@ -1,2 +1,33 @@
+# Assignment 1
+from football_dictionaries.assignment_1 import players_as_dictionaries
+    
+    
+
 def players_by_position(squads_list):
-    pass
+    
+    sq_list= players_as_dictionaries(squads_list)
+    
+    
+    tot_result = {}
+    
+    
+    for i in sq_list:        
+    
+        tot_result.setdefault('GK',[])
+        tot_result.setdefault('MF',[])
+        tot_result.setdefault('FW',[])
+        
+        if i['position'] == 'GK':
+            tot_result['GK'].append(i)
+            
+        if i['position'] == 'MF':
+            tot_result['MF'].append(i)
+            
+        if i['position'] == 'FW':
+            tot_result['FW'].append(i)
+            
+    return tot_result
+            
+            
+    
+    
