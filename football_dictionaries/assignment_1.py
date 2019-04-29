92f37f389002e17021a9e17f9beb0055a27cf8b2
zli715 <zli715@gmail.com>
2019-04-29 14:11:16 +0000

    Added functionality to football dictionaries; passing all tests

--- football_dictionaries/assignment_1.py
+++ football_dictionaries/assignment_1.py
@@ -1,2 +1,16 @@
 def players_as_dictionaries(squads_list):
-    pass
+    result = []
+    for player in squads_list:
+        result.append({
+            'number': player[0],
+            'position': player[1],
+            'name': player[2],
+            'date_of_birth': player[3],
+            'caps': player[4],
+            'club': player[5],
+            'country': player[6],
+            'club_country': player[7],
+            'year': player[8]
+        })
+    return result
+
