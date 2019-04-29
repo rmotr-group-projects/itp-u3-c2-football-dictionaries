691bf6a646f8a8f6cb5f06e1c96def1e7721c77f
daniellefuller00 <daniellefuller00@gmail.com>
2019-04-29 01:00:51 +0000

    added completed code

--- football_dictionaries/assignment_1.py
+++ football_dictionaries/assignment_1.py
@@ -1,2 +1,34 @@
+
 def players_as_dictionaries(squads_list):
-    pass
+    s_list = []
+    
+    for new_list in squads_list:
+        squad = {
+            'number': new_list[0],
+            'position': new_list[1],
+            'name': new_list[2],
+            'date_of_birth': new_list[3],
+            'caps': new_list[4],
+            'club': new_list[5],
+            'country': new_list[6],
+            'club_country': new_list[7],
+            'year': new_list[8]
+        }
+        s_list.append(squad)
+    return s_list
+        
+        
+'''
+{
+        'caps': '',
+        'club': 'Quilmes',
+        'club_country': 'Argentina',
+        'country': 'Argentina',
+        'date_of_birth': '(1908-10-23)23 October 1908 (aged 21)',
+        'name': 'Juan Botasso',
+        'number': '1',
+        'position': 'GK',
+        'year': '1930'
+    }
+'''        
+
--- football_dictionaries/assignment_2.py
+++ football_dictionaries/assignment_2.py
@@ -1,2 +1,15 @@
+from .assignment_1 import players_as_dictionaries
+
 def players_by_position(squads_list):
-    pass
+    squad = players_as_dictionaries(squads_list)
+    
+    player_position = {}
+    
+    for player in squad:
+        position = player['position']
+        player_position.setdefault(position, [])
+        player_position[position].append(player)
+    return player_position
+        
+        
+    
