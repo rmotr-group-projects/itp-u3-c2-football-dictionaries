8e65809324bc9cb7c328abb32065eaa74a5b58e8
roccocervantes <rcervantes77@gmail.com>
2019-04-29 17:54:18 +0000

    Finished football dictionaries assignment

--- football_dictionaries/assignment_2.py
+++ football_dictionaries/assignment_2.py
@@ -1,2 +1,11 @@
+from football_dictionaries.assignment_1 import players_as_dictionaries 
+
 def players_by_position(squads_list):
-    pass
+    final_dict = {}
+    player_list_of_dictionaries = players_as_dictionaries(squads_list)
+    
+    for player in player_list_of_dictionaries:
+        position = player["position"]
+        final_dict.setdefault(position, [])
+        final_dict[position].append(player)
+    return final_dict 
--- football_dictionaries/assignment_3.py
+++ football_dictionaries/assignment_3.py
@@ -1,2 +1,17 @@
+# Assignment 1
+from football_dictionaries.assignment_1 import players_as_dictionaries
+# Assignment 2
+from football_dictionaries.assignment_2 import players_by_position
+
 def players_by_country_and_position(squads_list):
-    pass
+    result = {} 
+    list_of_player_dictionaries = players_as_dictionaries(squads_list)
+    
+    for player in list_of_player_dictionaries:
+        country = player["country"]
+        position = player["position"]
+        result.setdefault(country, {})
+        result[country].setdefault(position,[])
+        result[country][position].append(player)
+        
+    return result 
