ab1472c51cdd46b0d40d01ba5348872c33ec3922
Sandra Herrera <sandra.h.arriaga@gmail.com>
2019-04-30 05:37:19 +0000

    adding functionality to football_dictionaries passed all tests

--- football_dictionaries/assignment_3.py
+++ football_dictionaries/assignment_3.py
@@ -1,2 +1,24 @@
 def players_by_country_and_position(squads_list):
-    pass
+    result = {}
+    
+    expected_countries = ['Argentina', 'Belgium', 'Brazil', 'South Korea']
+    
+    for player in squads_list:
+        result.setdefault(player[6],{})
+    
+    for player in squads_list:
+        result[player[6]].setdefault(player[1],[])
+        if player[6] in expected_countries:
+            result[player[6]][player[1]].append({
+                   'number': player[0],
+                   'position': player[1],
+                   'name': player[2],
+                   'date_of_birth': player[3],
+                   'caps': player[4],
+                   'club': player[5],
+                   'country': player[6],
+                   'club_country': player[7],
+                   'year': player[8]
+               })
+    print(result['Argentina'])           
+    return result
