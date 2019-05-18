fcb31dd7d9d2b6bae41f38d6cfc1f8ab3cd66d44
Stuart Whalley <50208733+swhalley9@users.noreply.github.com>
2019-05-18 08:33:05 -0600

    Update assignment_1.py

--- football_dictionaries/assignment_1.py
+++ football_dictionaries/assignment_1.py
@@ -1,2 +1,18 @@
 def players_as_dictionaries(squads_list):
-    pass
+    list_to_dict = []
+    for player in squads_list:
+        new_dict = {
+            'number' : player[0],
+            'position' : player[1],
+            'name' : player[2],
+            'date_of_birth' : player[3],
+            'caps' : player[4],
+            'club' : player[5],
+            'country' : player[6],
+            'club_country' : player[7],
+            'year' : player[8]
+            }
+        
+        list_to_dict.append(new_dict)
+        
+    return list_to_dict
