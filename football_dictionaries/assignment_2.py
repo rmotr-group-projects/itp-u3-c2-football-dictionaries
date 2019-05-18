4e51fd47cebe01b80f8eed63b92a099514260515
Stuart Whalley <50208733+swhalley9@users.noreply.github.com>
2019-05-18 08:43:50 -0600

    Update assignment_2.py

--- football_dictionaries/assignment_2.py
+++ football_dictionaries/assignment_2.py
@@ -1,2 +1,9 @@
 def players_by_position(squads_list):
-    pass
+    result_dict = players_as_dictionaries(squads_list)
+    result = {}
+    for player in result_dict:
+        position = player['position']
+        result.setdefault(position,[])
+        result[position].append(player)
+        
+    return result
