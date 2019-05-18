54e955ae7d3eaf67a08284ff793f0308c870668c
Stuart Whalley <50208733+swhalley9@users.noreply.github.com>
2019-05-18 09:01:54 -0600

    Update assignment_3.py

--- football_dictionaries/assignment_3.py
+++ football_dictionaries/assignment_3.py
@@ -1,2 +1,11 @@
 def players_by_country_and_position(squads_list):
-    pass
+    result_dict = players_as_dictionaries(squads_list)
+    result = {}
+    for players in result_dict:
+        countries = players['country']
+        result.setdefault(countries,{})
+        position = players['position']
+        result[countries].setdefault(position,[])
+        result[countries][position].append(players)
+        
+    return result
