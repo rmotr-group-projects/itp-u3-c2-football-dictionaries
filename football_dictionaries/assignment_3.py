b07419568c96e3cebf061dbfb1a37f1f39337322
Stuart Whalley <50208733+swhalley9@users.noreply.github.com>
2019-05-18 09:10:43 -0600

    Update assignment_3.py

--- football_dictionaries/assignment_3.py
+++ football_dictionaries/assignment_3.py
@@ -1,3 +1,5 @@
+from football_dictionaries.assignment_1 import players_as_dictionaries
+
 def players_by_country_and_position(squads_list):
     result_dict = players_as_dictionaries(squads_list)
     result = {}
