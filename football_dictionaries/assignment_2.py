4e237cc8f6134981c8d31124e3894694f149bf4d
Ilya Koy <Ilya.koy@outlook.com>
2019-05-24 07:38:01 -0400

    trying to figure out assignment_2

--- football_dictionaries/assignment_2.py
+++ football_dictionaries/assignment_2.py
@@ -1,2 +1,36 @@
+from squads_data import SQUADS_DATA
+
 def players_by_position(squads_list):
-    pass
+    players_as_dicts = {}
+    for player in squads_list:
+        if players_as_dicts and player[1] not in players_as_dicts:
+            players_as_dicts[player[1]] = [
+                {
+                'number': player[0],
+                'position': player[1],
+                'name': player[2],
+                'date_of_birth': player[3],
+                'caps': player[4],
+                'club': player[5],
+                'country': player[6],
+                'club_country': player[7],
+                'year': player[8]
+                }
+            ]
+        else:
+            players_as_dicts[player[1]].append(
+                {
+                'number': player[0],
+                'position': player[1],
+                'name': player[2],
+                'date_of_birth': player[3],
+                'caps': player[4],
+                'club': player[5],
+                'country': player[6],
+                'club_country': player[7],
+                'year': player[8]
+                }
+            )
+    return players_as_dicts
+
+players_by_position(SQUADS_DATA)
