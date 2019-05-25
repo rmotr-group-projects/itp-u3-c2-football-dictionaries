d186fcf3cb07f0d38a4433fef270aaae81409d7f
Gindi Khangura <khangura_9@hotmail.com>
2019-05-25 18:45:37 -0500

    Completed all assignments.

--- football_dictionaries/assignment_3.py
+++ football_dictionaries/assignment_3.py
@@ -1,2 +1,12 @@
+from football_dictionaries.assignment_2 import players_by_position
+
 def players_by_country_and_position(squads_list):
-    pass
+    players = players_by_position(squads_list)
+
+    country_position_dict = {}
+    for players_list in players.values():
+        for player in players_list:
+            country_position_dict.setdefault(player["country"], {})
+            country_position_dict[player["country"]].setdefault(player["position"], [])
+            country_position_dict[player["country"]][player["position"]].append(player)
+    return country_position_dict
