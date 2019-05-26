72ae9f87527799a87154395a0f3153b103fe0c80
icoban135 <ibrahimcoban1013@gmail.com>
2019-05-26 17:40:43 +0000

    Rmotr week 3 Football Dict Project

--- football_dictionaries/assignment_2.py
+++ football_dictionaries/assignment_2.py
@@ -1,2 +1,11 @@
+import football_dictionaries.assignment_1 as asd
+
 def players_by_position(squads_list):
-    pass
+    list_of_players = asd.players_as_dictionaries(squads_list)
+    player_dict = {}
+    for player in list_of_players: 
+        player_dict.setdefault(player['position'],[])
+        player_dict[player['position']].append(player)
+    return player_dict
+    
+ 
