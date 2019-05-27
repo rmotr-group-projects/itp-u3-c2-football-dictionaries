4ee12be6c095afbfe76567ba76399cd12083bab4
tjperry07 <tjperry07@gmail.com>
2019-05-26 22:51:06 -0400

    passed second test

--- football_dictionaries/assignment_2.py
+++ football_dictionaries/assignment_2.py
@@ -1,2 +1,11 @@
+from football_dictionaries.assignment_1 import players_as_dictionaries
+
+
 def players_by_position(squads_list):
-    pass
+    imported_players = players_as_dictionaries(squads_list)
+    player_position_dict = {}
+    for player in imported_players:
+        position = player['position']
+        player_position_dict.setdefault(position, [])
+        player_position_dict[position].append(player)
+    return player_position_dict
