d4b96b57daf5a7975ca4e4697606be67eec90891
Forrest <11200528+forrestm@users.noreply.github.com>
2019-05-28 06:07:41 -0500

    Updated assignment1.py

--- football_dictionaries/assignment_1.py
+++ football_dictionaries/assignment_1.py
@@ -1,2 +1,14 @@
 def players_as_dictionaries(squads_list):
-    pass
+    final_list = []
+    for item in squads_list:
+        final_dict = {'number': item[0],
+        'position': item[1],
+        'name': item[2],
+        'date_of_birth': item[3],
+        'caps': item[4],
+        'club': item[5],
+        'country': item[6],
+        'club_country': item[7],
+        'year': item[8]}
+        final_list.append(final_dict)
+    return final_list
