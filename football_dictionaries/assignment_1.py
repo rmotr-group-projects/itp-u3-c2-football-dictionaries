cd771cceaecc671a0e739a1ac67b893d773c7917
Alex-Skrivseth <alexskrivseth@gmail.com>
2019-05-28 05:33:51 +0000

    passing all tests

--- football_dictionaries/assignment_1.py
+++ football_dictionaries/assignment_1.py
@@ -1,2 +1,28 @@
+
+
 def players_as_dictionaries(squads_list):
-    pass
+    result = []
+    for player in squads_list:
+        empty_dict = {}
+        
+        for index,item in enumerate(player):
+            key_list = [
+            "number",
+            "position",
+            "name",
+            "date_of_birth",
+            "caps",
+            "club",
+            "country",
+            "club_country",
+            "year"
+            ]
+            key = key_list[index]
+            empty_dict[key] = item
+            if index == 8:
+                result.append(empty_dict)        
+    return result
+            
+    
+
+
--- football_dictionaries/assignment_2.py
+++ football_dictionaries/assignment_2.py
@@ -1,2 +1,22 @@
+from football_dictionaries.assignment_1 import players_as_dictionaries
+
 def players_by_position(squads_list):
-    pass
+    result = {
+        'GK': [],
+        'MF': [],
+        'FW': [],
+    }
+    GK_list = result["GK"]
+    MF_list = result["MF"]
+    FW_list = result["FW"]
+    players = players_as_dictionaries(squads_list)
+    
+    for player in players:
+        if player['position'] == 'GK':
+            GK_list.append(player)
+        elif player['position'] == 'MF':
+            MF_list.append(player)
+        else:
+            FW_list.append(player)
+    return result
+            
