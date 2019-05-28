add292972838622a8a6645f2b6e5a7c6e0f2e805
moodyg <geoff.moody@gmail.com>
2019-05-28 01:31:59 +0000

    completed code to pass tests for Football Dictionaries assignment - week 3 month 1 - data science track, worked solo with some assistance from Jason and Jordan.

--- football_dictionaries/assignment_2.py
+++ football_dictionaries/assignment_2.py
@@ -1,2 +1,39 @@
+from .assignment_1 import players_as_dictionaries
+
 def players_by_position(squads_list):
-    pass
+    #positions = players_as_dictionaries(squads_list)
+    positions_list = {}
+    for player in players_as_dictionaries(squads_list):
+            position = player['position']
+            
+            positions_list.setdefault(position, [])
+            positions_list[position].append(player)
+    
+    return positions_list
+        
+        
+        
+# positions_list = {
+       # position:  [
+            #Player data
+       # ] 
+# }  
+    
+    
+#from football_dictionaries.squads_data import SQUADS_DATA
+
+
+#def players_as_dictionaries(SQUADS_DATA):
+    #result = []
+    #for player in SQUADS_DATA:
+        #result.append({ 'caps': player[4],
+                        #'club': player[-4],
+                        #'club_country': player[-2],
+                        #'country': player[-3],
+                        #'date_of_birth': player[3],
+                        #'name': player[2],
+                        #'number': player[0],
+                        #'position': player[1],
+                        #'year': player[-1],
+                   # })
+    #return result
--- football_dictionaries/assignment_3.py
+++ football_dictionaries/assignment_3.py
@@ -1,2 +1,19 @@
+from .assignment_1 import players_as_dictionaries
+
 def players_by_country_and_position(squads_list):
-    pass
+    
+    country_list = {}
+    
+    for player in players_as_dictionaries(squads_list):
+        position = player['position']
+        country = player['country']    
+        
+        country_list.setdefault(country, {})
+        country_list[country].setdefault(position, [])
+        #access the list to append the player.
+        country_list[country][position].append(player)
+        
+       
+
+    return country_list
+        
