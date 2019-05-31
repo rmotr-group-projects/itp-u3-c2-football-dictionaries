de4573882649331f407ea1db365f7003d3bcbf72
Nanatetteh <nana.tetteh@gmail.com>
2019-05-31 02:31:26 +0000

    Code to for football disctionaries in assignment_[1,2,3].py

--- football_dictionaries/assignment_1.py
+++ football_dictionaries/assignment_1.py
@@ -1,2 +1,15 @@
 def players_as_dictionaries(squads_list):
-    pass
+    result = []
+    for player in squads_list:
+        playerinfo = {}
+        playerinfo['number'] = player[0]
+        playerinfo['position'] = player[1]
+        playerinfo['name'] = player[2]
+        playerinfo['date_of_birth'] = player[3]
+        playerinfo['caps'] = player[4]
+        playerinfo['club'] = player[5]
+        playerinfo['country'] = player[6]
+        playerinfo['club_country'] = player[7]
+        playerinfo['year'] = player[8]
+        result.append(playerinfo)
+    return result
