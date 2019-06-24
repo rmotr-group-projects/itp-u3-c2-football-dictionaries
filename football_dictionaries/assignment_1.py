571900e700aebe033161470e646665f06db4b478
realuniverse <mthounskane@gmail.com>
2019-06-24 04:01:06 +0000

    changed for assignments

--- football_dictionaries/assignment_1.py
+++ football_dictionaries/assignment_1.py
@@ -1,2 +1,15 @@
 def players_as_dictionaries(squads_list):
-    pass
+    squads_data = []
+    for data in range(len(squads_list)):
+        a_dict = {
+             'number': squads_list[data][0],
+             'position': squads_list[data][1],
+                'name': squads_list[data][2],
+          'date_of_birth': squads_list[data][3],
+                 'caps': squads_list[data][4],
+                'club': squads_list[data][5],
+              'country': squads_list[data][6],
+          'club_country': squads_list[data][7],
+                   'year': squads_list[data][8],}
+        squads_data.append(a_dict)
+    return squads_data
--- football_dictionaries/assignment_3.py
+++ football_dictionaries/assignment_3.py
@@ -1,2 +1,15 @@
+from football_dictionaries.assignment_2 import players_by_position
+
 def players_by_country_and_position(squads_list):
-    pass
+    positions = players_by_position(squads_list)
+    a_dict={}
+    for key, value in positions.items():
+        player1 = len(value)
+        for player in range(player1):
+            country = value[player]['country']
+            a_dict.setdefault(country,{})
+            a_dict[country].setdefault(key,[])
+            a_dict[country][key].append(positions[key][player]) 
+    return a_dict
+
+   
