c9a1d79de7b8f0f2d8040ed367208fe56af49564
manofoar <joetynan@gmail.com>
2019-06-25 03:19:35 +0000

    Futbol Project Submission

--- football_dictionaries/assignment_1.py
+++ football_dictionaries/assignment_1.py
@@ -1,2 +1,17 @@
-def players_as_dictionaries(squads_list):
-    pass
+
+def players_as_dictionaries(stuff):
+    players_list = []
+    number_of_players = len(stuff)
+    for player in range(number_of_players):
+        players_list.append({
+            'number':stuff[player][0],
+            'position':stuff[player][1],
+            'name':stuff[player][2],
+            'date_of_birth':stuff[player][3],
+            'caps':stuff[player][4],
+            'club':stuff[player][5],
+            'country':stuff[player][6],
+            'club_country': stuff[player][7],
+            'year':stuff[player][8]                           
+        })
+    return players_list
--- football_dictionaries/assignment_2.py
+++ football_dictionaries/assignment_2.py
@@ -1,2 +1,21 @@
+from football_dictionaries.assignment_1 import players_as_dictionaries
+
 def players_by_position(squads_list):
-    pass
+    player_list = players_as_dictionaries(squads_list)
+    
+    
+
+    positions = [] #unique list of positions
+    list_by_position = [[],[],[]] #list of players by position
+    number_of_players = len(player_list)
+#this loop creates a unique list of keys
+    for position in range(number_of_players):
+        if player_list[position]['position'] not in positions:
+            positions.append(player_list[position]['position'])     
+#this loop groups my players into three different lists based on position. let's try to make this a nested loop, creating a list of three lists.
+    for position_key in range(len(positions)):
+        for player in range(number_of_players):
+            if player_list[player]['position'] == positions[position_key]:
+                list_by_position[position_key].append(player_list[player])    
+    list_of_grouped_players = dict(zip(positions, list_by_position))
+    return list_of_grouped_players
