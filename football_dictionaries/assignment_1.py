800bfcb3fd808d1aa3a663e018b4b1f3e32b72e5
Dan Martz <dansdevelopments@gmail.com>
2019-07-01 12:00:45 -0500

    completed assignment 1
    wrote function players_as_dictionaries(squads_list)

--- football_dictionaries/assignment_1.py
+++ football_dictionaries/assignment_1.py
@@ -1,2 +1,15 @@
 def players_as_dictionaries(squads_list):
-    pass
+    squads_dict_list = []
+    for squad in squads_list:
+        squads_dict_list.append({
+                'number': squad[0],
+                'position': squad[1],
+                'name': squad[2],
+                'date_of_birth': squad[3],
+                'caps': squad[4],
+                'club': squad[5],
+                'country': squad[6],
+                'club_country': squad[7],
+                'year': squad[8],
+            })
+    return squads_dict_list
