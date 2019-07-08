3a6722628f9faebdeb854caad4289a99a8be2aa3
Isaiah <isaiahbanks500@gmail.com>
2019-07-08 18:28:07 +0000

    Worked on b34 to organize football list into a dictionariy football_ditionaries.py

--- football_dictionaries/assignment_1.py
+++ football_dictionaries/assignment_1.py
@@ -1,2 +1,22 @@
+from football_dictionaries.squads_data import SQUADS_DATA
+
 def players_as_dictionaries(squads_list):
-    pass
+    res = []
+    for x in squads_list:
+        res_dict = {
+            "caps":x[4],
+            "club":x[5],
+            "club_country":x[7],
+            "country":x[6],
+            "date_of_birth":x[3],
+            "name":x[2],
+            "number":x[0],
+            "position":x[1],
+            "year":x[8]
+        }
+        
+                
+        res.append(res_dict)     
+    return res
+            
+
--- football_dictionaries/assignment_3.py
+++ football_dictionaries/assignment_3.py
@@ -1,2 +1,14 @@
+from football_dictionaries.assignment_1 import players_as_dictionaries 
+
 def players_by_country_and_position(squads_list):
-    pass
+    x = players_as_dictionaries(squads_list)
+    z = {}
+    for i in x:
+        country = i["country"]
+        position = i["position"]
+        
+        z.setdefault(country,{})
+        z[country].setdefault(position,[])
+        
+        z[country][position].append(i)
+    return z
