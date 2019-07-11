830412611149ee386121951b2b63299028c70b39
eyebeards <dmonroeb@gmail.com>
2019-07-11 07:54:31 +0000

    Made changes to assignments 1,2 and 3 to pass all tests

--- football_dictionaries/assignment_3.py
+++ football_dictionaries/assignment_3.py
@@ -1,2 +1,18 @@
+from football_dictionaries.assignment_1 import players_as_dictionaries
+
 def players_by_country_and_position(squads_list):
-    pass
+    players = players_as_dictionaries(squads_list)
+    result = {}
+    
+    for player in players:
+        country = player['country']
+        position = player['position']
+        
+        if country not in result:
+            result[country] = {}
+        if position not in result[country]:
+            result[country][position] = []
+                
+        result[country][position].append(player)
+    
+    return result
