92487d5d4a68d588e5010ba5e0894977cdff4746
snyderj <jamielsnyder@gmail.com>
2019-07-22 20:13:43 +0000

    Completed all three assignments with Sara & Bobby

--- football_dictionaries/assignment_2.py
+++ football_dictionaries/assignment_2.py
@@ -1,2 +1,19 @@
 def players_by_position(squads_list):
-    pass
+    position_dict = {}
+    for player in squads_list:
+        position = player[1]
+        position_dict.setdefault(position, [])
+        player_dict = {
+            'number': player[0],
+            'position': player[1],
+            'name': player[2],
+            'date_of_birth': player[3],
+            'caps': player[4],
+            'club': player[5],
+            'country': player[6],
+            'club_country': player[7],
+            'year': player[8],
+        }
+        position_dict[position].append(player_dict)
+    return position_dict
+        
