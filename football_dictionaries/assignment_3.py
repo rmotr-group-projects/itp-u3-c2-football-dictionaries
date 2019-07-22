8b01eeeeacf4a09f0f68c40721b931c8c7c00509
tytea <tytea36@gmail.com>
2019-07-22 00:40:33 +0000

    revised

--- football_dictionaries/assignment_3.py
+++ football_dictionaries/assignment_3.py
@@ -1,3 +1,24 @@
+
+def players_as_dictionaries(data):
+    playerslist = []
+    
+    for player in data:
+        playersdict = {
+    'number': player[0],
+    'position': player[1],
+    'name': player[2],
+    'date_of_birth': player[3],
+    'caps': player[4],
+    'club': player[5],
+    'country': player[6],
+    'club_country': player[7],
+    'year': player[8],
+}
+        playerslist.append(playersdict)
+    
+    return playerslist
+
+
 def players_by_country_and_position(data):
     results = {}
     
