cf3e5e1fc6dcc32dc6eb2e8442e416ede0f84a93
Jwashington <jcwashin77@gmail.com>
2019-07-22 01:34:42 +0000

    Added changes to football_dictionaries.py to pass tests and submit project 1

--- football_dictionaries/assignment_1.py
+++ football_dictionaries/assignment_1.py
@@ -1,2 +1,19 @@
 def players_as_dictionaries(squads_list):
-    pass
+    squads_data = []
+    player_data = {}
+    
+    for player in squads_list:
+        player_data = {
+            'caps': player[4],
+            'club': player[-4],
+            'club_country': player[-2],
+            'country': player[-3],
+            'date_of_birth': player[3],
+            'name': player[2],
+            'number': player[0],
+            'position': player[1],
+            'year': player[-1]        
+        }
+        squads_data.append(player_data)
+    return squads_data
+        
--- football_dictionaries/assignment_2.py
+++ football_dictionaries/assignment_2.py
@@ -1,2 +1,16 @@
+
+
+
+from football_dictionaries.assignment_1 import players_as_dictionaries
+
 def players_by_position(squads_list):
-    pass
+    squads_data = players_as_dictionaries(squads_list)
+    result = {}
+    
+    for player in squads_data:
+        position = player['position']
+        result.setdefault(position, [])
+        result[position].append(player)
+        
+    return result
+        
