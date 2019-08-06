396fb3def8b09c4fa1415e3b669f715ebaf6f58d
iris9112 <iris9112@gmail.com>
2019-08-06 10:16:37 -0500

    ISABEL RUIZ 06-08-2019

--- football_dictionaries/assignment_1.py
+++ football_dictionaries/assignment_1.py
@@ -1,2 +1,22 @@
 def players_as_dictionaries(squads_list):
-    pass
+
+    dict_squads_list = []
+    player_dict = {}
+    
+    for player in squads_list:
+        player_dict = {
+            'number': player[0],
+            'position': player[1],
+            'name': player[2],
+            'date_of_birth': player[3],
+            'caps': player[4],
+            'club': player[5],
+            'country': player[6],
+            'club_country': player[7],
+            'year': player[8],
+        }
+    
+        dict_squads_list.append(player_dict)
+
+    return dict_squads_list
+
--- football_dictionaries/assignment_3.py
+++ football_dictionaries/assignment_3.py
@@ -1,2 +1,18 @@
+from football_dictionaries.assignment_1 import players_as_dictionaries
+
+
 def players_by_country_and_position(squads_list):
-    pass
+
+    squads_list = players_as_dictionaries(squads_list)
+    result = {}
+    
+    for player in squads_list:
+        position = player['position']
+        country = player['country']
+        result.setdefault(country, {})
+        result[country].setdefault(position, [])
+        result[country][position].append(player)
+    
+    return result
+
+        
